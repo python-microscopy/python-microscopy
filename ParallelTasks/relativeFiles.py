a6f030b5f8d9f725b6dfc2f269a75c3bf6d7a832
David Baddeley <david.baddeley.nz@gmail.com>
2008-07-29 06:09:59 +0000

    *** empty log message ***

---
+++ ParallelTasks/relativeFiles.py
@@ -0,0 +1,31 @@
+import os
+
+def getFullFilename(relFilename):
+    ''' returns a fully resolved filename given a filename relative to 
+    the environment variable PYMEDATADIR. If environment variable not defined,
+    assumes path is absolute.'''
+
+    if 'PYMEDATADIR' in os.environ.keys():
+        return os.path.join(os.environ['PYMEDATADIR'], relFilename)
+    else:
+        return relFilename
+
+
+def getRelFilename(filename):
+    '''returns the tail of filename'''
+    
+    #first make sure we have an absolute path
+    filename = os.path.expanduser(filename)
+    if not os.path.isabs(filename):
+        filename= os.path.abspath(filename)
+
+    if 'PYMEDATADIR' in os.environ.keys():
+        dataDir = os.environ['PYMEDATADIR']
+        if not dataDir[-1] in [os.sep, os.altsep]:
+            dataDir = dataDir + os.sep
+
+        if filename.startswith(dataDir): #if we've selected something which isn't under our data directory we're going to have to stick with an absolute path
+            return filename[len(dataDir):]
+
+    return filename
+
