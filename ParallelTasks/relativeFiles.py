69dbbb62e4d92088eed0d348bcb18a8a97587fb6
David Baddeley <david.baddeley.nz@gmail.com>
2008-07-31 05:48:49 +0000

    *** empty log message ***

--- ParallelTasks/relativeFiles.py
+++ ParallelTasks/relativeFiles.py
@@ -1,9 +1,14 @@
 import os
+import string
+
+def translateSeparators(filename):
+    return string.translate(filename, string.maketrans('\\/', os.sep + os.sep))
 
 def getFullFilename(relFilename):
     ''' returns a fully resolved filename given a filename relative to 
     the environment variable PYMEDATADIR. If environment variable not defined,
     assumes path is absolute.'''
+    relFilename = translateSeparators(relFilename)
 
     if 'PYMEDATADIR' in os.environ.keys():
         return os.path.join(os.environ['PYMEDATADIR'], relFilename)
@@ -13,6 +18,7 @@ def getFullFilename(relFilename):
 
 def getRelFilename(filename):
     '''returns the tail of filename'''
+    filename = translateSeparators(filename)
     
     #first make sure we have an absolute path
     filename = os.path.expanduser(filename)
