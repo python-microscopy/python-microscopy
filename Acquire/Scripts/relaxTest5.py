aaf2c928b8ae9bc0bf72f6dcc02a04fbf9c1c29e
David Baddeley <david.baddeley.nz@gmail.com>
2008-08-11 22:17:20 +0000

    *** empty log message ***

--- Acquire/Scripts/relaxTest5.py
+++ Acquire/Scripts/relaxTest5.py
@@ -1,22 +1,26 @@
 #scope.pa.start()
 
 #frs.OnBStartSpoolButton(None)
+import copy
+
+l = copy.copy(l488)#get a copy of laser to resolve pyro threading issues
+l.dd = copy.copy(l.dd)
 
 time.sleep(0.1)
-l488.TurnOn()
+l.TurnOn()
 time.sleep(10)
 
 onTime = 10
 offTimes = 20*ones(10)
 
 for offT in offTimes:
-    l488.TurnOff()
+    l.TurnOff()
     time.sleep(offT)
-    l488.TurnOn()
+    l.TurnOn()
     time.sleep(onTime)
 
 
-l488.TurnOff()
+l.TurnOff()
 scope.pa.stop()
 
 #frs.OnBStopSpoolingButton(None)
