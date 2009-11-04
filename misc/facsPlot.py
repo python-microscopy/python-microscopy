d87db092c22d8d28c0ea347deb26be72011632c4
David Baddeley <david.baddeley.nz@gmail.com>
2009-11-04 01:28:27 +0000

    *** empty log message ***

--- misc/facsPlot.py
+++ misc/facsPlot.py
@@ -24,6 +24,25 @@ def facsPlotScatter(x, y, nbins=None, ms=1):
 
     scatter(x, y, c=c, s=ms, edgecolors='none')
 
+def facsPlotScatterLog(x, y, nbins=None, ms=1):
+    if nbins == None:
+        nbins = 0.25*sqrt(len(x))
+    n, xedge, yedge = histogram2d(x, y, bins = [nbins,nbins], range=[(min(x), max(x)), (min(y), max(y))])
+
+    dx = diff(xedge[:2])
+    dy = diff(yedge[:2])
+
+    c = ndimage.map_coordinates(n, [(x - xedge[0])/dx, (y - yedge[0])/dy])
+
+    scatter(x, y, c=c, s=ms, edgecolors='none')
+
+    ax = gca()
+    ax.get_xaxis().set_scale('log')
+    ax.get_yaxis().set_scale('log')
+    draw()
+    
+    
+
 def facsPlotContour(x, y, nbins=None):
     if nbins == None:
         nbins = 0.25*sqrt(len(x))
