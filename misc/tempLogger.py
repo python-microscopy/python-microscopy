16b5f6197c581c92c0655eab93671c5ee154acd6
David Baddeley <david.baddeley.nz@gmail.com>
2010-06-25 01:23:06 +0000

    no message

---
+++ misc/tempLogger.py
@@ -0,0 +1,12 @@
+from PYME.Acquire.Hardware.DigiData.DigiDataClient import getDDClient
+import time
+
+dd = getDDClient()
+
+f = open('/home/david/tempLog.txt', 'a')
+
+while True:
+    v = dd.GetAIValue(1)*1000./2.**15
+    f.write('%3.2f\t%3.2f\n' % (time.time(), v))
+    f.flush()
+    time.sleep(1)
