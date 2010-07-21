45060bf638ff3584ce3e6cc837d78bf430eb79aa
David Baddeley <david.baddeley.nz@gmail.com>
2010-07-21 22:28:36 +0000

    no message

--- misc/tempLogger.py
+++ misc/tempLogger.py
@@ -3,10 +3,10 @@ import time
 
 dd = getDDClient()
 
-f = open('/home/david/tempLog.txt', 'a')
+f = open('/home/david/tempLog_10s.txt', 'a')
 
 while True:
     v = dd.GetAIValue(1)*1000./2.**15
     f.write('%3.2f\t%3.2f\n' % (time.time(), v))
     f.flush()
-    time.sleep(1)
+    time.sleep(10)
