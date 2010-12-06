285fe6425e2197bc9be112c1cb0c8493d74db938
David Baddeley <david.baddeley.nz@gmail.com>
2010-12-06 17:02:53 +1300

    initial jython array dummies

---
+++ JythonDummies/numpy/core/multiarray.py
@@ -0,0 +1,22 @@
+import array
+
+class ndarray:
+    def __init__(self, shape, typecode):
+        self.shape = shape
+        self.dtype = dtype(typecode)
+
+    def __setstate__(self, ver, shape, dtype, isfortran, rawdata):
+        self.shape = shape
+        self.dtype = dtype
+
+        self.array = array.array(self.dtype, rawdata)
+
+class dtype:
+    def __init__(self, obj, align=False, copy=Flase):
+        self.typestr = obj
+
+    def __setstate__(self, ):
+
+def _reconstruct(cls, dims, typecode):
+    return cls(dims, typecode)
+
