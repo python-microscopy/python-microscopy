48514b9f2fd9af4d78424a5bce25c2b187a93898
David Baddeley <david.baddeley.nz@gmail.com>
2010-12-06 17:03:29 +1300

    stubs for drift correction functions to allow visualisation to compile

---
+++ Analysis/qHull/lhoodStubs.c
@@ -0,0 +1,30 @@
+/*
+##################
+# lhoodStubs.c
+#
+# Copyright David Baddeley, 2010
+# d.baddeley@auckland.ac.nz
+#
+# This file may NOT be distributed without express permision from David Baddeley
+#
+# Stubs without drift correction logic so that the rendering part of the code still compiles
+#
+##################
+ */
+
+#include "triangLhood.h"
+
+int calcLHood (coordT *points, int dim, int numpoints, double *lsum) {
+  int exitcode = 0;             /* 0 if no error from qhull */
+  return exitcode;
+}
+
+int calcLHood2D (coordT *points, int numpoints, double *lsum) {
+  int exitcode = 0;             /* 0 if no error from qhull */
+  return exitcode;
+}
+
+int calcLHood3D (coordT *points, int numpoints, double *lsum) {
+  int exitcode = 0;             /* 0 if no error from qhull */
+  return exitcode;
+}
--- Analysis/qHull/setup.py
+++ Analysis/qHull/setup.py
@@ -1,14 +1,29 @@
+import os
+
 qhullSources = ['user.c', 'global.c', 'stat.c', 'io.c', 'geom2.c', 'poly2.c',
        'merge.c', 'geom.c', 'poly.c', 'qset.c', 'mem.c', 'usermem.c', 'userprintf.c', 'rboxlib.c','random.c','libqhull.c']
 
 qhullSources = ['qhull/' + s for s in qhullSources]
 
 def configuration(parent_package = '', top_path = None):
-    from numpy.distutils.misc_util import Configuration, get_numpy_include_dirs
+    from numpy.distutils.misc_util import Configuration, get_numpy_include_dirs, yellow_text
     config = Configuration('triangWrap', parent_package, top_path)
 
+    #print 'foo'
+    #print yellow_text('foo' + config.local_path)
+
+    srcs = ['triangWrap.c', 'triangRend.c', '../SoftRend/drawTriang.c']
+
+    #check for drift correction code
+    if os.path.exists(os.path.join(config.local_path, 'triangLhood.c')):
+        print yellow_text('compiling with drift correction')
+        srcs.append('triangLhood.c')
+    else:
+        print yellow_text('compiling without drift correction')
+        srcs.append('lhoodStubs.c')
+
     config.add_extension('triangWrap',
-        sources=['triangWrap.c', 'triangLhood.c', 'triangRend.c', '../SoftRend/drawTriang.c'] + qhullSources,
+        sources=srcs + qhullSources,
         include_dirs = get_numpy_include_dirs()+['qhull', '../SoftRend'],
 	extra_compile_args = ['-O3', '-fno-exceptions', '-ffast-math'])
 
