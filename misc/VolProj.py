017e3eac8a62eff84e540ae025838bfd849622d9
David Baddeley <david.baddeley.nz@gmail.com>
2011-02-10 09:01:58 +1300

    isosurface stuff

--- misc/VolProj.py
+++ misc/VolProj.py
@@ -10,9 +10,19 @@
 ##################
 import pylab
 from enthought.mayavi import mlab
+import os
+import tempfile
+from PYME.FileUtils.readTiff import read3DTiff
 
+def GetImage(figure):
+    tfname = os.path.join(tempfile.gettempdir(), 'mvi_out.tif')
+    f.scene.save_tiff(tfname)
 
+    im = read3DTiff(tfname).squeeze()
 
+    os.remove(tfname)
+
+    return im
 
 
 class Isosurf:
@@ -20,22 +30,23 @@ class Isosurf:
         self.f = mlab.figure()
 
         self.isos = []
-
+        self.projs = []
 
         for im, th, i in zip(channels, thresholds, range(len(channels))):
-            projs
+            ps = []
 
             pr = im.mean(2)
-            zproj.append((255*pylab.minimum(pr/(1.5*th), 1)).astype('uint8'))
+            ps.append(self.drawProjection((255*pylab.minimum(pr/(1.5*th), 1)).astype('uint8'), 'z'))
             pr = im.mean(0)
-            xproj.append((255*pylab.minimum(pr/(1.5*th), 1)).astype('uint8'))
+            ps.append(self.drawProjection((255*pylab.minimum(pr/(1.5*th), 1)).astype('uint8'), 'x'))
             pr = im.mean(1)
-            yproj.append((255*pylab.minimum(pr/(1.5*th), 1)).astype('uint8'))
+            ps.append(self.drawProjection((255*pylab.minimum(pr/(1.5*th), 1)).astype('uint8'), 'y'))
+
+            self.projs.append(ps)
 
             c = mlab.contour3d(im, contours=[th], color = pylab.cm.gist_rainbow(float(i)/len(channels))[:3])
             c.mlab_source.dataset.spacing = pixelsize
             self.isos.append(c)
-
 
 
     def drawProjection(im, axis):
@@ -47,3 +58,11 @@ class Isosurf:
         gp.actor.property.representation = 'surface'
 
         return gp
+
+    def ShowProj(self, chan, show=True):
+        for p in self.projs[chan]:
+            p.visible = show
+
+    def ShowIso(self, show=True):
+        for c in self.isos:
+            c.visible = True
