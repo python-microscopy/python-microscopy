58e21acb531cd64b575f31236d3e046c9db92842
David Baddeley <david.baddeley.nz@gmail.com>
2011-04-05 11:48:59 +1200

    merge

---
+++ Analysis/DataSources/BufferedDataSource.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+##################
+# BufferedDataSource.py
+#
+# Copyright David Baddeley, 2011
+# d.baddeley@auckland.ac.nz
+# 
+# This file may NOT be distributed without express permision from David Baddeley
+#
+##################
+import numpy
+
+class DataSource: #buffer our io to avoid decompressing multiple times
+    def __init__(self,dataSource, bLen = 12):
+        self.bLen = bLen
+        self.buffer = None #delay creation until we know the dtype
+        #self.buffer = numpy.zeros((bLen,) + dataSource.getSliceShape(), 'uint16')
+        self.insertAt = 0
+        self.bufferedSlices = -1*numpy.ones((bLen,), 'i')
+        self.dataSource = dataSource
+
+    def getSlice(self,ind):
+        #global bufferMisses
+        #print self.bufferedSlices, self.insertAt, ind
+        #return self.dataSource.getSlice(ind)
+        if ind in self.bufferedSlices: #return from buffer
+            #print int(numpy.where(self.bufferedSlices == ind)[0])
+            return self.buffer[int(numpy.where(self.bufferedSlices == ind)[0]),:,:]
+        else: #get from our data source and store in buffer
+            sl = self.dataSource.getSlice(ind)
+            self.bufferedSlices[self.insertAt] = ind
+
+            if self.buffer == None: #buffer doesn't exist yet
+                self.buffer = numpy.zeros((self.bLen,) + self.dataSource.getSliceShape(), sl.dtype)
+
+            self.buffer[self.insertAt, :,:] = sl
+            self.insertAt += 1
+            self.insertAt %=self.bLen
+
+            #bufferMisses += 1
+
+            return sl
+
+    def getSliceShape(self):
+        #return (self.im.size[1], self.im.size[0])
+        return self.dataSource.getSliceShape()
+        #return self.data.shape[:2]
+
+    def getNumSlices(self):
+        return self.dataSource.getNumSlices()
+
+    def getEvents(self):
+        return self.dataSource.getEvents()
+
+    def release(self):
+        return self.dataSource.release()
+        
+
+    def reloadData(self):
+        return self.dataSource.reloadData()
+        
+
+
