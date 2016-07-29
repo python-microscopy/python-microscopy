2c92b02d2b67fe966486b8e6472cda7f33549776
Andrew Barentine <andrew.barentine@yale.edu>
2016-07-29 19:33:05 -0400

    Adding preliminary biplane mapping module to LMVis extras.

---
+++ PYME/LMVis/Extras/biplaneMapping.py
@@ -0,0 +1,77 @@
+
+import wx
+import numpy as np
+from PYME.Analysis.points import twoColour
+
+def foldX(pipeline):
+    """
+    At this point the origin of x should be the corner of the concatenated frame
+    """
+    xOG = pipeline['x']
+    roiSizeNM = (pipeline.mdh['Multiview.ROISize'][1]*pipeline.mdh['voxelsize.x']*1000)  # voxelsize is in um
+    xfold = xOG % roiSizeNM
+    mvQuad = np.floor(xOG / roiSizeNM)
+
+    pipeline.mapping.setMapping('xFolded', xfold)
+    pipeline.mapping.setMapping('multiviewQuadrant', mvQuad)
+    return
+
+def pairMolecules(xFold, y):
+    numMol = len(xFold)
+    dist = np.zeros((numMol, numMol))
+    for ind in range(len(dist)):
+        for ii in range(len(dist)):
+            dist[ind, ii] = np.sqrt((xFold[ind] - xFold[ii])**2 + (y[ind] - y[ii])**2)
+    dist = dist + (dist == 0)*999999
+    minDist = dist.min(axis=0)
+    minLoc = dist.argmin(axis=0)
+
+    import matplotlib.pyplot as plt
+    plt.figure()
+    plt.scatter(xFold, y)
+    for ii in range(numMol):
+        plt.plot([xFold[ii], xFold[minLoc[ii]]], [y[ii], y[minLoc[ii]]], color='red')
+    #plt.show()
+
+class biplaneMapper:
+    def __init__(self, visFr):
+        self.visFr = visFr
+
+        ID_FOLD_BIPLANE = wx.NewId()
+        visFr.extras_menu.Append(ID_FOLD_BIPLANE, "Fold and Stack Biplane")
+        visFr.Bind(wx.EVT_MENU, self.OnFoldBiplane, id=ID_FOLD_BIPLANE)
+
+    def OnFoldBiplane(self, event):
+        pipeline = self.visFr.pipeline
+
+        try:
+            numROI = pipeline.mdh['Multiview.NumROIs']
+        except KeyError:
+            raise UserWarning('You are either not looking at Biplane Data, or your metadata is incomplete')
+
+        foldX(pipeline)
+
+        print('length is %i, max is %d' % (len(pipeline.mapping.__dict__['xFolded']), np.max(pipeline.mapping.__dict__['xFolded'])))
+        print('Number of ROIs: %f' % np.max(pipeline.mapping.__dict__['multiviewQuadrant']))
+        self.plotRegistered(pipeline.mapping.__dict__['xFolded'], pipeline['y'],
+                            pipeline.mapping.__dict__['multiviewQuadrant'], 'Raw')
+
+        # Now we need to match up molecules
+        pairMolecules(pipeline.mapping.__dict__['xFolded'], pipeline['y'])
+
+
+    def plotRegistered(self, regX, regY, multiviewChannels, title=''):
+        import matplotlib.pyplot as plt
+        nChan = multiviewChannels.max()
+
+        c = iter(plt.cm.rainbow(np.linspace(0, 1, nChan)))
+        for ii in range(nChan):
+            mask = (ii == multiviewChannels)
+            plt.scatter(regX[mask], regY[mask], c=next(c))
+        plt.title(title)
+        return
+
+
+def Plug(visFr):
+    """Plugs this module into the gui"""
+    biplaneMapper(visFr)
