a19bcdbfddb6db11ff7230635ac7e8cc23e4ad54
Andrew Barentine <andrew.barentine@yale.edu>
2016-07-31 11:03:26 -0400

    Outlined preliminary shiftmap application

--- PYME/LMVis/Extras/biplaneMapping.py
+++ PYME/LMVis/Extras/biplaneMapping.py
@@ -16,6 +16,17 @@ def foldX(pipeline):
     pipeline.mapping.setMapping('whichFOV', mvQuad)
     return
 
+def plotRegistered(regX, regY, multiviewChannels, title=''):
+    import matplotlib.pyplot as plt
+    nChan = multiviewChannels.max()
+
+    c = iter(plt.cm.rainbow(np.linspace(0, 1, nChan)))
+    for ii in range(nChan):
+        mask = (ii == multiviewChannels)
+        plt.scatter(regX[mask], regY[mask], c=next(c))
+    plt.title(title)
+    return
+
 def pairMolecules(xFold, y, FOV):
     numMol = len(xFold)
     dist = np.zeros((numMol, numMol))
@@ -26,7 +37,8 @@ def pairMolecules(xFold, y, FOV):
     minDist = dist.min(axis=0)
     minLoc = dist.argmin(axis=0)
 
-    keepList = np.where(FOV[minLoc] != FOV)
+    # only keep molecule pairs heterozygous in planes and who are mutually nearest neighbors
+    keepList = np.where(np.logical_and(FOV[minLoc] != FOV, minLoc[range(numMol)] == minLoc[minLoc[range(numMol)]]))
     '''
     keepList = []  # np.zeros(numMol, dtype=bool)
     for ind in range(numMol):
@@ -49,39 +61,68 @@ class biplaneMapper:
     def __init__(self, visFr):
         self.visFr = visFr
 
-        ID_FOLD_BIPLANE = wx.NewId()
-        visFr.extras_menu.Append(ID_FOLD_BIPLANE, "Fold and Stack Biplane")
-        visFr.Bind(wx.EVT_MENU, self.OnFoldBiplane, id=ID_FOLD_BIPLANE)
+        ID_REGISTER_BIPLANE = wx.NewId()
+        visFr.extras_menu.Append(ID_REGISTER_BIPLANE, "Biplane - Register Channels")
+        visFr.Bind(wx.EVT_MENU, self.OnRegisterBiplane, id=ID_REGISTER_BIPLANE)
 
-    def OnFoldBiplane(self, event):
+        ID_MAP_BIPLANE = wx.NewId()
+        visFr.extras_menu.Append(ID_MAP_BIPLANE, "Biplane - Map Z")
+        visFr.Bind(wx.EVT_MENU, self.OnFoldAndMap, id=ID_MAP_BIPLANE)
+        return
+
+    def applyShiftmaps(self, xFolded, y, whichFOV, numFOV):
         pipeline = self.visFr.pipeline
 
-        try:
-            numROI = pipeline.mdh['Multiview.NumROIs']
+        xReg = xFolded + [(whichFOV == ii)*self.shiftMapsX[ii](xFolded) for ii in range(numFOV)]
+        yReg = y + [(whichFOV == ii)*self.shiftMapsY[ii](y) for ii in range(numFOV)]
+
+        pipeline.mapping.setMapping('xReg', xReg)
+        pipeline.mapping.setMapping('yReg', yReg)
+        return
+
+    def OnFoldAndMap(self, event):
+        pipeline = self.visFr.pipeline
+
+        try:  # load shiftmaps, if present
+            numFOV = pipeline.mdh['Multiview.NumROIs']
+            self.shiftMapsX = [pipeline.mdh['shiftMapsX.FOV%d' % ii] for ii in range(numFOV)]
+            self.shiftMapsY = [pipeline.mdh['shiftMapsY.FOV%d' % ii] for ii in range(numFOV)]
         except KeyError:
-            raise UserWarning('You are either not looking at Biplane Data, or your metadata is incomplete')
+            raise UserWarning('Shiftmaps or number of FOVs not found in metadata')
+            return
+            #self.viewOrigins = [croppingInfo['Multiview.ROI%dOrigin' % i] for i in range(self.numROIs)]
 
         foldX(pipeline)
 
         print('length is %i, max is %d' % (len(pipeline.mapping.__dict__['xFolded']), np.max(pipeline.mapping.__dict__['xFolded'])))
         print('Number of ROIs: %f' % np.max(pipeline.mapping.__dict__['whichFOV']))
-        self.plotRegistered(pipeline.mapping.__dict__['xFolded'], pipeline['y'],
+        plotRegistered(pipeline.mapping.__dict__['xFolded'], pipeline['y'],
+                            pipeline.mapping.__dict__['whichFOV'], 'Raw')
+
+        self.applyShiftmaps(pipeline.mapping.__dict__['xFolded'], pipeline['y'],
+                            pipeline.mapping.__dict__['whichFOV'], numFOV)
+
+    def OnRegisterBiplane(self, event):
+        pipeline = self.visFr.pipeline
+
+        try:
+            numFOV = pipeline.mdh['Multiview.NumROIs']
+        except KeyError:
+            raise UserWarning('You are either not looking at Biplane Data, or your metadata is incomplete')
+            return
+
+        foldX(pipeline)
+
+        print('length is %i, max is %d' % (len(pipeline.mapping.__dict__['xFolded']), np.max(pipeline.mapping.__dict__['xFolded'])))
+        print('Number of ROIs: %f' % np.max(pipeline.mapping.__dict__['whichFOV']))
+        plotRegistered(pipeline.mapping.__dict__['xFolded'], pipeline['y'],
                             pipeline.mapping.__dict__['whichFOV'], 'Raw')
 
         # Now we need to match up molecules
         pairMolecules(pipeline.mapping.__dict__['xFolded'], pipeline['y'], pipeline.mapping.__dict__['whichFOV'])
 
 
-    def plotRegistered(self, regX, regY, multiviewChannels, title=''):
-        import matplotlib.pyplot as plt
-        nChan = multiviewChannels.max()
 
-        c = iter(plt.cm.rainbow(np.linspace(0, 1, nChan)))
-        for ii in range(nChan):
-            mask = (ii == multiviewChannels)
-            plt.scatter(regX[mask], regY[mask], c=next(c))
-        plt.title(title)
-        return
 
 
 def Plug(visFr):
