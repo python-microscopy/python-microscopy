71e7f20f3f425a6ed85c8f15d99b88dc68e188ff
Andrew Barentine <andrew.barentine@yale.edu>
2016-08-10 19:59:25 -0400

    added handling in inpFilt to un-nest data types with multiple entries per element

--- PYME/LMVis/Extras/multiviewMapping.py
+++ PYME/LMVis/Extras/multiviewMapping.py
@@ -189,10 +189,10 @@ def astigMAPism(pipeline, stigLib, chanPlane):
                 wy = 1./fres['fitError']['sigmayPlane%i' % chanPlane[ii]][mi]**2
                 errX = wx*(fres['fitResults']['sigmaxPlane%i' % chanPlane[ii]][mi] - sigCalX['chan%i' % ii])**2
                 errY = wy*(fres['fitResults']['sigmayPlane%i' % chanPlane[ii]][mi] - sigCalY['chan%i' % ii])**2
-                wx = 1./fres['fitError']['sigmaxPlane%i' % chanPlane[ii]][mi]**2
-                wy = 1./fres['fitError']['sigmayPlane%i' % chanPlane[ii]][mi]**2
-                errX = wx*(fres['fitResults']['sigmaxPlane%i' % chanPlane[ii]][mi] - sigCalX['chan%i' % ii])**2
-                errY = wy*(fres['fitResults']['sigmayPlane%i' % chanPlane[ii]][mi] - sigCalY['chan%i' % ii])**2
+                #wx = 1./fres['fitError']['sigmaxPlane%i' % chanPlane[ii]][mi]**2
+                #wy = 1./fres['fitError']['sigmayPlane%i' % chanPlane[ii]][mi]**2
+                #errX = wx*(fres['fitResults']['sigmaxPlane%i' % chanPlane[ii]][mi] - sigCalX['chan%i' % ii])**2
+                #errY = wy*(fres['fitResults']['sigmayPlane%i' % chanPlane[ii]][mi] - sigCalY['chan%i' % ii])**2
                 # find minimum
                 try:
                     z[mi] = zVal[np.nanargmin(errX + errY)]
