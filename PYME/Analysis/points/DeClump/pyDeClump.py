fixed bug where NaNs were replacing zero sigmaPlane%i values of localizations in ignored color channels, resulting in NaNs in weighted averaging when these ignored color channels were now the color channels of interest.

PYME/Analysis/points/DeClump/pyDeClump.py
@@ -197,7 +197,8 @@ def coalesceClumps(fitResults, assigned, okChans=None):
             #vn = np.zeros(1,dtype=dt)
                         
             fres['tIndex'][i] = tIs[ci].min()
-
+            if i == 189:
+                2+2
             #fres['fitResults'][i], fres['fitError'][i] = weightedAverage(vals['fitResults'], vals['fitError'])
             fres['fitResults'][i], fres['fitError'][i] = weightedAverage_(rvs, evs, dtr)
 

PYME/LMVis/Extras/multiviewMapping.py
@@ -543,9 +543,11 @@ class multiviewMapper:
 
         for cind in np.unique(chanColor):
             fresCopy = fresCopy[(fresCopy['tIndex'].argsort())]
+            # make sure NaNs (awarded when there is no sigma in a given plane of a clump) do not carry over from when
+            # ignored channel localizations were clumped by themselves
             for pp in range(numPlanes):
-                fresCopy['fitResults']['sigmaxPlane%i' % pind][np.isnan(fresCopy['fitResults']['sigmaxPlane%i' % pind])] = 0
-                fresCopy['fitResults']['sigmayPlane%i' % pind][np.isnan(fresCopy['fitResults']['sigmayPlane%i' % pind])] = 0
+                fresCopy['fitResults']['sigmaxPlane%i' % pp][np.isnan(fresCopy['fitResults']['sigmaxPlane%i' % pp])] = 0
+                fresCopy['fitResults']['sigmayPlane%i' % pp][np.isnan(fresCopy['fitResults']['sigmayPlane%i' % pp])] = 0
             # trick pairMolecules function by tweaking the channel vector
             planeInColorChan = np.copy(fresCopy['whichChannel'])
             chanColor = np.array(chanColor)