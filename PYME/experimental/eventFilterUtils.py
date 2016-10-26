94b60d64c821c44b99eed1a8abf06ef9e2ad0f15
Andrew Barentine <andrew.barentine@yale.edu>
2016-10-25 21:12:47 -0400

    reworked idTransientFrames to accept events as dictionary or recarray

--- PYME/experimental/eventFilterUtils.py
+++ PYME/experimental/eventFilterUtils.py
@@ -13,12 +13,16 @@ def idTransientFrames(dataSource, events, fps):
         nothing, but adds column to input datasource
 
     """
-    # fps = mdh['StackSettings.FramesPerStep']  # frames per step
-    focusChanges = events[events['EventName'] == 'ProtocolFocus']
+    # need this next bit to work if events is a dictionary or a recarray:
+    names = np.array(events['EventName'])
+    descr = np.array(events['EventDescr'])
+    focusChanges = descr[names == 'ProtocolFocus']
+
     # for ProtocolFocus events, description is 'frame#, position'
     t = np.copy(dataSource['t'])
     ti = np.ones_like(t, dtype=int)
-    for fi in focusChanges['EventDescr']:
+
+    for fi in focusChanges:
         fi = float(fi.split(',')[0])  # only interested in frame# at the moment
         ti[np.logical_and(t >= fi, t < fi+fps)] -= 1
 
