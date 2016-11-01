48de1b303b1fd0a15ee5e292690382df59dd21b5
David Baddeley <david.baddeley.nz@gmail.com>
2016-11-01 11:30:17 -0400

    tweaks to chunk settings for pytables

--- PYME/IO/dataExporter.py
+++ PYME/IO/dataExporter.py
@@ -79,7 +79,7 @@ class H5Exporter(Exporter):
         self.complevel = complevel
 
     def Export(self, data, outFile, xslice, yslice, zslice, metadata=None, events = None, origName=None, progressCallback=None):
-        h5out = tables.openFile(outFile,'w')
+        h5out = tables.openFile(outFile,'w', chunk_cache_size=2**23)
         filters=tables.Filters(self.complevel,self.complib,shuffle=True)
 
         nframes = (zslice.stop - zslice.start)/zslice.step
@@ -91,7 +91,7 @@ class H5Exporter(Exporter):
         #atm = tables.UInt16Atom()
         atm = tables.Atom.from_dtype(data[xslice, yslice, 0].dtype)
 
-        ims = h5out.createEArray(h5out.root,'ImageData',atm,(0,xSize,ySize), filters=filters, expectedrows=nframes)#, chunkshape=(1,xSize,ySize))
+        ims = h5out.createEArray(h5out.root,'ImageData',atm,(0,xSize,ySize), filters=filters, expectedrows=nframes, chunkshape=(1,xSize,ySize))
 
         curFrame = 0
         for frameN in range(zslice.start,zslice.stop, zslice.step):
