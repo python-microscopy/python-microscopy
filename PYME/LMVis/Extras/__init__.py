f510ee1b0d4c5c69df0fb8da5d55d8c30bab4c90
Kenny Chung <kenny.chung@yale.edu>
2016-11-15 17:12:37 -0500

    Glob statement too imprecise, picking up ???.py.orig files made by Mercurial

--- PYME/LMVis/Extras/__init__.py
+++ PYME/LMVis/Extras/__init__.py
@@ -26,7 +26,7 @@ import os.path
 import glob
 import os
 
-mods = list(set([os.path.splitext(os.path.split(p)[-1])[0] for p in glob.glob(__path__[0] + '/[a-zA-Z]*.py*')]))
+mods = list(set([os.path.splitext(os.path.split(p)[-1])[0] for p in glob.glob(__path__[0] + '/[a-zA-Z]*.py') + glob.glob(__path__[0] + '/[a-zA-Z]*.pyc')]))
 
 def InitPlugins(visFr):
     
