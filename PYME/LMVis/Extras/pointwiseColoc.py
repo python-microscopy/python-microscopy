798222f55b15fc33dcc69671f92a6d35fd9017da
David Baddeley <david.baddeley.nz@gmail.com>
2016-11-15 11:52:42 -0500

    added pointwise colocalization module

---
+++ PYME/LMVis/Extras/pointwiseColoc.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+##################
+# pointwiseColoc.py
+#
+# Copyright David Baddeley, 2010
+# d.baddeley@auckland.ac.nz
+# 
+# This file may NOT be distributed without express permision from David Baddeley
+#
+##################
+
+#import wx
+
+class PointwiseColocaliser:
+    def __init__(self, visFr):
+        self.visFr = visFr
+
+        visFr.AddMenuItem('Extras', "Pointwise Colocalisation", self.OnPointwiseColoc)
+        
+    def OnPointwiseColoc(self, event):
+        #from PYME import mProfile
+        #mProfile.profileOn(['distColoc.py'])
+        from PYME.Analysis.Colocalisation import distColoc
+        #A vs B
+        distColoc.calcDistCorr(self.visFr.colourFilter, *(self.visFr.colourFilter.getColourChans()[::1]))
+        #B vs A
+        #distColoc.calcDistCorr(self.visFr.colourFilter, *(self.visFr.colourFilter.getColourChans()[::-1]))
+        #mProfile.report()
+
+
+
+def Plug(visFr):
+    '''Plugs this module into the gui'''
+    PointwiseColocaliser(visFr)
+
+
