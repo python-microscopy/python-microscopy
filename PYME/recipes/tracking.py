22c1a7715fb91e9dab5a4d9a2aa62333f041509d
David Baddeley <david.baddeley.nz@gmail.com>
2016-11-29 15:03:15 -0500

    bugfixes for recipe changes

--- PYME/recipes/tracking.py
+++ PYME/recipes/tracking.py
@@ -5,14 +5,13 @@ Created on Tue Dec 29 19:59:15 2015
 @author: david
 """
 from .base import register_module, ModuleBase,Filter
-from .traits import Input, Output, Float, Enum, CStr, Bool, Int, DictStrFloat, DictStrBool
+from .traits import Input, Output, Float, Enum, CStr, Bool, Int, DictStrFloat, DictStrBool, on_trait_change
 
 import numpy as np
 import pandas as pd
 
 from PYME.Analysis.Tracking import tracking
 from PYME.Analysis.Tracking import trackUtils
-from traits.api import on_trait_change
 
 from PYME.IO import tabular
 
--- PYME/recipes/traits.py
+++ PYME/recipes/traits.py
@@ -1,4 +1,5 @@
 """ Shim to give us a uniform way of importing traits (and replacing them if needed in the future)"""
+from __future__ import absolute_import
 
 try:
     from enthought.traits.api import *
