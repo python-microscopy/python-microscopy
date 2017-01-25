bugfix in nearestneighbour distances where distance between each point and itself was being returned if calculation was for a single color. GUI call updated as well

PYME/LMVis/Extras/clusterAnalysis.py
@@ -86,6 +86,7 @@ class ClusterAnalyser:
             if not chan_dlg.ShowModal() == wx.ID_OK:
                 return  # handle cancel
 
+            dkey = 'neighbourDists'
             selectedChans = chan_dlg.GetSelections()
             if len(selectedChans) == 2:
                 # select order
@@ -117,15 +118,19 @@ class ClusterAnalyser:
                 # restore original display settings
                 self.pipeline.colourFilter.setColour(dispColor)
 
+                matchMaker = measurement.NearestNeighbourDistances(columns=['x', 'y', 'z'], inputChan0='A', inputChan1='B',
+                                                               outputName='output', key=dkey)
+            elif len(selectedChans) > 2:
+                raise RuntimeError('NearestNeighbour cannot handle more than two channels')
+
+
         if len(selectedChans) < 2:
             desc = 'Single channel nearest neighbours'
-            namespace = {'A': self.pipeline, 'B': self.pipeline}
-        elif len(selectedChans) > 2:
-            raise RuntimeError('NearestNeighbour cannot handle more than two channels')
+            namespace = {'A': self.pipeline}
+            matchMaker = measurement.NearestNeighbourDistances(columns=['x', 'y', 'z'], inputChan0='A', inputChan1='',
+                                                               outputName='output', key=dkey)
 
-        dkey = 'neighbourDists'
-        matchMaker = measurement.NearestNeighbourDistances(columns=['x', 'y', 'z'], inputChan0='A', inputChan1='B',
-                                                           outputName='output',key=dkey)
+
         matchMaker.execute(namespace)
         dists = np.array(namespace['output'][dkey])
 

PYME/recipes/measurement.py
@@ -264,15 +264,22 @@ class NearestNeighbourDistances(ModuleBase):
         
         if self.inputChan1 == '':
             pos1 = pos
+            singleChan = True  # flag to not pair molecules with themselves
         else:
             pos1 = namespace[self.inputChan1]
+            singleChan = False
 
         #create a kdtree
         p1 = np.vstack([pos[k] for k in self.columns]).T
         p2 = np.vstack([pos1[k] for k in self.columns]).T
         kdt = cKDTree(p1)
 
-        d, i = kdt.query(p2, 1)
+        if singleChan:
+            #query the two closest entries - the closest entry will be the orig point paired with itself, so ignore it
+            d, i = kdt.query(p2, 2)
+            d = d[:, 1]
+        else:
+            d, i = kdt.query(p2, 1)
 
         res = pd.DataFrame({self.key: d})
         if 'mdh' in dir(pos):