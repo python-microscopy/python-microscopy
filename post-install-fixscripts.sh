313575f72b389667d68decd412a2a537e04b897c
Christian Soeller <c.soeller@exeter.ac.uk>
2017-01-30 20:50:17 +0000

    updated mac postinstall

--- post-install-fixscripts.sh
+++ post-install-fixscripts.sh
@@ -1,9 +1,9 @@
 PYMEVER=`python -c 'import PYME.version ; print PYME.version.version'`
 PYPREF=`python -c 'import sys; print sys.prefix'`
-for filename in dh5view VisGUI fitMonP
+for filename in dh5view VisGUI fitMonP dh5view.py VisGUI.py
 do
     echo "patching $PYPREF/bin/$filename..."
-    sed -i '' -E 's|^#!(.*)python$|#!/bin/bash \1\pythonw|' $PYPREF/bin/$filename
+    #sed -i '' -E 's|^#!(.*)python$|#!/bin/bash \1\pythonw|' $PYPREF/bin/$filename
     sed -i '' -E "s/PYME==[0-9.]+/PYME==$PYMEVER/" $PYPREF/bin/$filename
 done
 
