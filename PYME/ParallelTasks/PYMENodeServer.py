2f8576a3cf0cd6c0a2267afa3b9da5212c2228ba
Andrew Barentine <andrew.barentine@yale.edu>
2017-02-12 11:59:14 -0500

    adding a comment

--- PYME/ParallelTasks/PYMENodeServer.py
+++ PYME/ParallelTasks/PYMENodeServer.py
@@ -63,10 +63,10 @@ def main():
     #remove old log files
     try:
         os.remove(os.path.join(nodeserver_log_dir, 'nodeserver.log'))
-    except OSError:  # if
+    except OSError:  # if we cant clear out old log files, we might not have a log directory set up
         try:
             os.makedirs(os.path.join(nodeserver_log_dir))  # NB - this will create all intermediate directories as well
-        except:
+        except:  # throw error because the RotatingFileHandler will fail to initialize
             raise IOError('Unable to initialize log files at %s' % nodeserver_log_dir)
         pass
     
