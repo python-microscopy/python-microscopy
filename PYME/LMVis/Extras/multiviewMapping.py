0b381a5a77c4fc6586a9ec70ec0e956b63482295
Andrew Barentine <andrew.barentine@yale.edu>
2017-02-20 18:12:07 -0500

    changing CheckAstigCalibration to use focus key, which should now be available if any z-stepping was performed with proper metadata/events.

--- PYME/LMVis/Extras/multiviewMapping.py
+++ PYME/LMVis/Extras/multiviewMapping.py
@@ -465,6 +465,7 @@ class multiviewMapper:
     def OnCheckAstigCalibration(self, event=None):
         """
         For use with dyes on a coverslip.
+        NB localizations from transient frames should be filtered prior to calling this function
 
         Parameters
         ----------
@@ -477,18 +478,16 @@ class multiviewMapper:
         import matplotlib.pyplot as plt
 
         pipeline = self.pipeline
-        try:
-            # NB localizations from transient frames should be filtered prior to calling this function
-            fpstep = pipeline.mdh['StackSettings.FramesPerStep']
-            stepSize = pipeline.mdh['StackSettings.StepSize'] * 1000  # convert um to nm
-        except AttributeError:
-            raise UserWarning('CheckAstigCalibration requires StackSettings metadata: NumCycles, NumSteps, FramesPerStep')
-
         # sort localizations according to frame
         I = np.argsort(pipeline['t'])
         zsort = pipeline['astigZ'][I]
-        step = np.divide(pipeline['t'].astype(int)[I], (fpstep*np.ones_like(zsort)).astype(int))
-        uni, counts = np.unique(step, return_counts=True)
+        try:
+            focus = pipeline['focus'][I]
+        except AttributeError:
+            raise UserWarning('CheckAstigCalibration requires ProtocolFocus events OR StackSettings metadata (StepSize, FramesPerStep)')
+
+        # find unique of ints to avoid floating point issues
+        uni, counts = np.unique(focus.astype(int), return_counts=True)
 
         cycles = len(uni)
         mu = np.empty(cycles)
@@ -500,7 +499,7 @@ class multiviewMapper:
         plt.ylabel('Counts')
 
         indi = 0
-        for ci in uni:
+        for ci in range(len(uni)):
             indf = indi + counts[ci]
             slicez = zsort[indi:indf]
             mu[ci] = np.mean(slicez)
@@ -510,15 +509,17 @@ class multiviewMapper:
             plt.hist(slicez, bins=range(int(min(slicez)), int(max(slicez) + 0.5*stdDev[ci]), int(0.5*stdDev[ci])), color=nc)
 
             indi = indf
-        #plt.figure()
-        #plt.scatter(mu, uni)
+
         plt.figure()
         diffs = mu[:-1] - mu[1:]
-        plt.bar(uni[:-1], diffs)
+        plt.bar(uni[:-1], diffs, width=(0.75*(uni[1] - uni[0])))
         plt.ylabel('Separation between steps')
 
         plt.figure()
-        plt.plot([stepSize, stepSize], [0, 1], color='black', label='Target Separation')
+        try:
+            plt.plot(pipeline.mdh['StackSettings.StepSize']*np.array([1, 1]), [0, 1], color='black', label='Target Separation')
+        except AttributeError:
+            pass
         plt.scatter(diffs, 0.5*np.ones_like(diffs), label='Separations')
         plt.title('Std deviation of step separations = %.1f' % np.std(diffs))
         plt.legend(scatterpoints=1, loc=2)
