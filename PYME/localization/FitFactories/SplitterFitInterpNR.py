54a99ec2404d303df92fd5f779fb08605e797918
David Baddeley <david.baddeley.nz@gmail.com>
2017-03-06 14:40:04 -0500

    added GUI support for using .tif for PSFs on OSX (previously only posible on Win and linux)

--- PYME/localization/FitFactories/SplitterFitInterpNR.py
+++ PYME/localization/FitFactories/SplitterFitInterpNR.py
@@ -330,7 +330,7 @@ from PYME.localization.FitFactories import Interpolators
 from PYME.localization.FitFactories import zEstimators
 
 PARAMETERS = [mde.ChoiceParam('Analysis.InterpModule','Interp:','CSInterpolator', choices=Interpolators.interpolatorList, choiceNames=Interpolators.interpolatorDisplayList),
-              mde.FilenameParam('PSFFile', 'PSF:', prompt='Please select PSF to use ...', wildcard='PSF Files|*.psf'),
+              mde.FilenameParam('PSFFile', 'PSF:', prompt='Please select PSF to use ...', wildcard='PSF Files|*.psf|TIFF files|*.tif'),
               mde.ShiftFieldParam('chroma.ShiftFilename', 'Shifts:', prompt='Please select shiftfield to use', wildcard='Shiftfields|*.sf'),
               #mde.IntParam('Analysis.DebounceRadius', 'Debounce r:', 4),
               mde.FloatParam('Analysis.AxialShift', 'Z Shift [nm]:', -270),
