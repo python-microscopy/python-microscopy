50708a12a0c1860f1fe3fcb8d7c952956597c9cc
David Baddeley <david.baddeley.nz@gmail.com>
2017-03-23 22:16:26 +0000

    HTTPTaskPusher.py edited online with Bitbucket

--- PYME/ParallelTasks/HTTPTaskPusher.py
+++ PYME/ParallelTasks/HTTPTaskPusher.py
@@ -92,7 +92,7 @@ class HTTPTaskPusher(object):
         clusterResults.fileResults(self.resultsURI + '/MetaData', metadata)
         clusterResults.fileResults(self.resultsURI + '/Events', self.ds.getEvents())
 
-        # TODO - check if this is redundant after calling fileResults for metadata
+        # set up metadata file which is used for deciding how to launch the analysis
         clusterIO.putFile(resultsMDFilename, self.mdh.to_JSON(), serverfilter=serverfilter)
         
         #wait until clusterIO caches clear to avoid replicating the results file.
