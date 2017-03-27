a9dccd2137b5584d1f2112363a30a8c626b7ec75
Andrew Barentine <andrew.barentine@yale.edu>
2017-03-27 16:56:49 -0400

    handling for cluster sizes of < 3, or any other cluster without 3 principle axes

--- PYME/Analysis/points/cluster_morphology.py
+++ PYME/Analysis/points/cluster_morphology.py
@@ -99,14 +99,21 @@ def measure_3d(x, y, z, output=None):
     
     #radius of gyration
     output['gyrationRadius'] = np.sqrt(np.mean(x_*x_ + y_*y_ + z_*z_))
-    
+
     #principle axes
     u, s, v = np.linalg.svd(np.vstack([x_, y_, z_]).T)
-    
-    for i in range(3):
-        output['axis%di' % i], output['axis%dj' % i], output['axis%dk' % i] = v[i]
-        #std. deviation along axes
-        output['sigma%d' % i] = s[i]/np.sqrt(N-1)
+
+    try:
+        for i in range(3):
+            output['axis%di' % i], output['axis%dj' % i], output['axis%dk' % i] = v[i]
+            #std. deviation along axes
+            output['sigma%d' % i] = s[i]/np.sqrt(N-1)
+    except IndexError:  # this occurs if e.g. the cluster is only 2 points, and only has two principle axes
+        # zero all svd outputs
+        for i in range(3):
+            output['axis%di' % i], output['axis%dj' % i], output['axis%dk' % i], output['sigma%d' % i] = 0, 0, 0, 0
+            output['theta'], output['phi'] = 0, 0
+        return output
     
     pa = v[0]
     #angle of principle axis
