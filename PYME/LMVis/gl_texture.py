f7913acaee2649bfd5ea8251dad7400b300526a2
Michael Graff <michael.graff@yale.edu>
2017-03-29 09:06:25 -0400

    rename parameter in texture

--- PYME/LMVis/gl_texture.py
+++ PYME/LMVis/gl_texture.py
@@ -50,7 +50,7 @@ class GaussTexture:
         glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT)
         glUniform1i(uniform_location, 0)
 
-    def load_texture(self, size=31, sigma=5, is_sum_one=False):
+    def load_texture(self, size=31, sigma=5, normalize_sum=False):
         """
         This method create a gauss kernel texture and stores it on the graphics card.
         The max amplitude of the kernel is 1.
@@ -69,8 +69,8 @@ class GaussTexture:
         index = next(x[0] for x in enumerate(data[size // 2, :]) if x[1] > 0.5)
         size_correction_factor = (float(size) / 2) / float(size // 2 - index)
 
-        if is_sum_one:
-            data /= sum(data.flatten())
+        if normalize_sum:
+            data /= data.sum()
         glGenTextures(1, self._texture_id)
         glTexImage2D(GL_TEXTURE_2D, 0, GL_LUMINANCE, size, size, 0, GL_LUMINANCE, GL_FLOAT, np.float16(data))
 
