1fad39b4220dc2afad3cb9695c853f88a916b7bb
Michael Graff <michael.graff@yale.edu>
2017-04-03 13:39:04 -0400

    repaired snapshot plugin

--- PYME/LMVis/Extras/snapshot.py
+++ PYME/LMVis/Extras/snapshot.py
@@ -19,16 +19,22 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 import PIL
+from cv2 import imshow
+
+from scipy.misc import toimage
 from wx import wx
 
 
 def save_snapshot(canvas):
-    width, height = canvas.Size[0], canvas.Size[1]
-    img = PIL.Image.frombytes('RGB', (width, height), canvas.getIm().tostring())
+    img = toimage(canvas.getIm().transpose(1, 0, 2))
     img = img.transpose(PIL.Image.FLIP_TOP_BOTTOM)
+    img.show()
     file_name = wx.FileSelector('Save Image as ... (image .png will be appended to filename)')
     if file_name:
-        img.save('{}.png'.format(file_name))
+        if not file_name.endswith('.png'):
+            img.save('{}.png'.format(file_name))
+        else:
+            img.save('{}'.format(file_name))
 
 
 def Plug(vis_fr):
