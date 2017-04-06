ff911f607f42e22d684b23050c4e2d86d21ce19d
Michael Graff <michael.graff@yale.edu>
2017-04-05 20:05:40 -0400

    extracted view functions from the menu to the panel

--- PYME/LMVis/Extras/ViewPanel.py
+++ PYME/LMVis/Extras/ViewPanel.py
@@ -18,6 +18,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+import json
 
 import wx
 import wx.lib.agw.aui as aui
@@ -38,19 +39,28 @@ class ViewPanel(wx.Panel):
         self.SetSizerAndFit(vertical_sizer)
 
     def create_buttons(self, vertical_sizer):
-        grid_sizer = wx.GridSizer(3, 1)
+        grid_sizer = wx.GridSizer(6, 1)
         x_button = wx.Button(self, -1, label='View in x', style=wx.BU_EXACTFIT)
         y_button = wx.Button(self, -1, label='View in y', style=wx.BU_EXACTFIT)
         z_button = wx.Button(self, -1, label='View in z', style=wx.BU_EXACTFIT)
 
+        skip = wx.StaticText(self, -1, label='', style=wx.BU_EXACTFIT)
+        save_button = wx.Button(self, -1, label='Save View', style=wx.BU_EXACTFIT)
+        load_button = wx.Button(self, -1, label='Load View', style=wx.BU_EXACTFIT)
+
         self.Bind(wx.EVT_BUTTON, self.set_view_x, x_button)
         self.Bind(wx.EVT_BUTTON, self.set_view_y, y_button)
         self.Bind(wx.EVT_BUTTON, self.set_view_z, z_button)
+        self.Bind(wx.EVT_BUTTON, self.save_view, save_button)
+        self.Bind(wx.EVT_BUTTON, self.load_view, load_button)
 
-        grid_sizer.Add(x_button)
-        grid_sizer.Add(y_button)
-        grid_sizer.Add(z_button)
-        vertical_sizer.Add(grid_sizer)
+        grid_sizer.Add(x_button, flag=wx.EXPAND)
+        grid_sizer.Add(y_button, flag=wx.EXPAND)
+        grid_sizer.Add(z_button, flag=wx.EXPAND)
+        grid_sizer.Add(skip, flag=wx.EXPAND)
+        grid_sizer.Add(save_button, flag=wx.EXPAND)
+        grid_sizer.Add(load_button, flag=wx.EXPAND)
+        vertical_sizer.Add(grid_sizer, flag=wx.EXPAND)
 
     def set_view_x(self, event):
         vec_up = [0, 1, 0]
@@ -85,6 +95,22 @@ class ViewPanel(wx.Panel):
         self.alternate = not self.alternate
         new_view = View(old_view.view_id, vec_up, vec_back, vec_right, old_view.translation, old_view.zoom)
         self.get_canvas().set_view(new_view)
+
+    def save_view(self, event):
+        view = self.get_canvas().get_view()
+        file_name = wx.FileSelector('Save view as json named... ')
+        if file_name:
+            if not file_name.endswith('.json'):
+                file_name = '{}.json'.format(file_name)
+            with open(file_name, 'w') as f:
+                f.writelines(json.dumps(view.to_json(), indent=4))
+
+    def load_view(self, event):
+        file_name = wx.FileSelector('Open View-JSON file')
+        if file_name:
+            with open(file_name, 'r') as f:
+                view = View.decode_json(json.load(f))
+            self.get_canvas().set_view(view)
 
     def get_canvas(self):
         return self.parent_panel.glCanvas
