6e8bce7a505193696dc3efe5b70670b7b6fab96b
Andrew Barentine <andrew.barentine@yale.edu>
2017-09-01 13:35:18 -0400

    in FileChucker, explicitly wait for both events file and zsteps file, also avoid cpu busy loop while waiting

--- PYME/experimental/dcimgFileChucker.py
+++ PYME/experimental/dcimgFileChucker.py
@@ -121,9 +121,10 @@ class venerableFileChucker(object):
         spool_timeout = time.time() + self.timeout
         while (not spooling_complete) and (time.time() < spool_timeout):
             # check to see if we have an events file (our end signal)
-            on_disk = (os.path.exists(events_filename) or os.path.exists(zsteps_filename))
+            on_disk = (os.path.exists(events_filename) and os.path.exists(zsteps_filename))
             if only_spool_complete and not on_disk:
                 # keep waiting
+                time.sleep(0.1)
                 continue
             else:
                 # flag as complete so we exit the loop after spooling
