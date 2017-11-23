16d81e029047d48a0469bab5743225d53192dd3f
David Baddeley <david.baddeley.nz@gmail.com>
2017-11-24 09:06:16 +1300

    added a more bakeshopesque recipe runner to clusterUI

--- clusterUI/recipes/urls.py
+++ clusterUI/recipes/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     url(r'^simple_form/$', views.recipe_form, name='recipe'),
     url(r'^run/$', views.run, name='runrecipe'),
     url(r'^svg/$', views.view_svg, name='svg'),
+    url(r'^standalone/$', views.recipe_standalone, name='bakeshop'),
+    url(r'^find_inputs/$', views.get_input_glob, name='find_inputs'),
 ]
