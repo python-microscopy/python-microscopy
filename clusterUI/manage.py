d3d220269c30617b19752a998c7f39ef7a21c84b
David Baddeley <david.baddeley.nz@gmail.com>
2018-02-28 08:24:24 +1300

    profiling update

--- clusterUI/manage.py
+++ clusterUI/manage.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
 import os
 import sys
-from PYME.util import fProfile
+from PYME.util import fProfile, mProfile
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "clusterUI.settings")
 
     from django.core.management import execute_from_command_line
 
-    prof = fProfile.thread_profiler()
-    prof.profileOn('.*clusterUI.*', '/home/ubuntu/clusterUI_prof.txt')
+    #prof = fProfile.thread_profiler()
+    #prof.profileOn('.*clusterUI.*', '/home/ubuntu/clusterUI_prof.txt')
+    mProfile.profileOn(['clusterIO.py',])
     execute_from_command_line(sys.argv)
-    prof.profileOff()
+    mProfile.profileOff()
+    mProfile.report(False, '/home/ubuntu/')
+    #prof.profileOff()
