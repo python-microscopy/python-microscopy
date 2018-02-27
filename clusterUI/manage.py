6a97fc57363f3347f8894cabe8b46813e75b526e
David Baddeley <david.baddeley.nz@gmail.com>
2018-02-28 08:03:58 +1300

    added profiling to clusterUI

--- clusterUI/manage.py
+++ clusterUI/manage.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python
 import os
 import sys
+from PYME.util import fProfile
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "clusterUI.settings")
 
     from django.core.management import execute_from_command_line
 
+    prof = fProfile.thread_profiler()
+    prof.profileOn('.*clusterUI.*', '/home/ubuntu/clusterUI_prof.txt')
     execute_from_command_line(sys.argv)
+    prof.profileOff()
