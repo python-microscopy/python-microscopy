13902afa4fefaecb1c8ea4102a8232272865bde2
Andrew Barentine <andrew.barentine@yale.edu>
2018-02-26 20:42:08 -0500

    move loop exit via exception to be closer to where it actually occurs

--- PYME/ParallelTasks/taskWorkerHTTP.py
+++ PYME/ParallelTasks/taskWorkerHTTP.py
@@ -244,48 +244,49 @@ class taskWorker(object):
             # new style way of returning results to reduce load on server
             from PYME.IO import clusterResults
 
-            try:
-                while True:
-                    #print 'getting results'
+            while True:  # loop over results queue until it's empty
+                #print 'getting results'
+                try:
                     queueURL, taskDescr, res = self.resultsQueue.get_nowait()
                     outputs = taskDescr.get('outputs', {})
+                except Queue.Empty:
+                    # queue is empty
+                    break
 
-                    try:
-                        if res is None:
-                            #failure
-                            s = clusterIO._getSession(queueURL)
-                            r = s.post(queueURL + 'node/handin?taskID=%s&status=failure' % taskDescr['id'])
-                            if not r.status_code == 200:
-                                logger.error('Returning task failed with error: %s' % r.status_code)
-                        elif res == True: #isinstance(res, ModuleCollection): #recipe output
-                            #res.save(outputs) #abuse outputs dictionary as context
+                try:
+                    if res is None:
+                        #failure
+                        s = clusterIO._getSession(queueURL)
+                        r = s.post(queueURL + 'node/handin?taskID=%s&status=failure' % taskDescr['id'])
+                        if not r.status_code == 200:
+                            logger.error('Returning task failed with error: %s' % r.status_code)
+                    elif res == True: #isinstance(res, ModuleCollection): #recipe output
+                        #res.save(outputs) #abuse outputs dictionary as context
 
-                            s = clusterIO._getSession(queueURL)
-                            r = s.post(queueURL + 'node/handin?taskID=%s&status=success' % taskDescr['id'])
-                            if not r.status_code == 200:
-                                logger.error('Returning task failed with error: %s' % r.status_code)
+                        s = clusterIO._getSession(queueURL)
+                        r = s.post(queueURL + 'node/handin?taskID=%s&status=success' % taskDescr['id'])
+                        if not r.status_code == 200:
+                            logger.error('Returning task failed with error: %s' % r.status_code)
 
+                    else:
+                        #success
+                        if 'results' in outputs.keys():
+                            #old style pickled results
+                            clusterResults.fileResults(outputs['results'], res)
                         else:
-                            #success
-                            if 'results' in outputs.keys():
-                                #old style pickled results
-                                clusterResults.fileResults(outputs['results'], res)
-                            else:
-                                if len(res.results) > 0:
-                                    clusterResults.fileResults(outputs['fitResults'], res.results)
+                            if len(res.results) > 0:
+                                clusterResults.fileResults(outputs['fitResults'], res.results)
 
-                                if len(res.driftResults) > 0:
-                                    clusterResults.fileResults(outputs['driftResults'], res.driftResults)
+                            if len(res.driftResults) > 0:
+                                clusterResults.fileResults(outputs['driftResults'], res.driftResults)
 
-                            s = clusterIO._getSession(queueURL)
-                            r = s.post(queueURL + 'node/handin?taskID=%s&status=success' % taskDescr['id'])
-                            if not r.status_code == 200:
-                                logger.error('Returning task failed with error: %s' % r.status_code)
-                    except requests.Timeout as e:
-                        logger.exception('Returning task failed on timeout.')
+                        s = clusterIO._getSession(queueURL)
+                        r = s.post(queueURL + 'node/handin?taskID=%s&status=success' % taskDescr['id'])
+                        if not r.status_code == 200:
+                            logger.error('Returning task failed with error: %s' % r.status_code)
+                except requests.Timeout as e:
+                    logger.exception('Returning task failed on timeout.')
 
-            except Queue.Empty:
-                pass
 
             if not self._loop_alive:
                 break
