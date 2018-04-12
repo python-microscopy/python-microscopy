fa47ad7f57b2381344392c0372f7dabbd5ec86d0
Ruisheng Lin <r.lin@exeter.ac.uk>
2018-04-12 14:47:50 +0100

    fix gen3DTriangs import error

--- PYME/LMVis/Layer/WireFrameRenderLayer.py
+++ PYME/LMVis/Layer/WireFrameRenderLayer.py
@@ -20,7 +20,7 @@
 #
 from PYME.LMVis.Layer.RenderLayer import RenderLayer
 from PYME.LMVis.ShaderProgram.WireFrameShaderProgram import WireFrameShaderProgram
-from PYME.LMVis.gen3DTriangs import gen3DTriangs
+from PYME.Analysis.points import gen3DTriangs
 from OpenGL.GL import *
 
 
