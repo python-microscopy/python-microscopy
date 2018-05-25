1f28e2512a29d8b6aec93d9771bc8b285cd0b1f2
David Baddeley <david.baddeley.nz@gmail.com>
2018-05-25 16:40:25 +1200

    Preliminary code for driving a galvo off a soundcard.

---
+++ PYME/experimental/scanner_control.py
@@ -0,0 +1,95 @@
+'''
+Drive a galvo scanner from a raspberry pi and expose a web interface
+'''
+
+import webframework
+import numpy as np
+import pygame
+import sys
+
+import logging
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+class ScannerController(object):
+    def __init__(self, scan_frequency=100, scan_amplitude=1.0):
+        self._scan_frequency = scan_frequency
+        self._scan_amplitude = scan_amplitude
+        
+        self._sound = None
+        
+        self._fullscale = 2**15
+        
+        pygame.mixer.pre_init(44100, size=-16, channels=1)
+        pygame.init()
+        
+    @webframework.register_endpoint('/start_scanning')
+    def start_scanning(self, frequency=None, amplitude=None, waveform='sine'):
+        if not self._sound is None:
+            self._sound.stop()
+            self._sound = None
+            
+        if frequency is None:
+            frequency = self._scan_frequency
+        else:
+            frequency = float(frequency)
+        
+        if amplitude is None:
+            amplitude = self._scan_amplitude
+        else:
+            amplitude = float(amplitude)
+            
+        
+        num_samples_per_period = (44100.0/frequency)
+        
+        #take at least 2 periods
+        num_periods = max(np.ceil(frequency), 2.0)
+        
+        t = np.linspace(0, num_periods/frequency, int(num_periods*num_samples_per_period))
+        
+        if waveform == 'sine':
+            y = amplitude*self._fullscale*np.sin(2*np.pi*frequency*t)
+        else:
+            raise  RuntimeError('waveform %s not supported' % waveform)
+        
+        
+        self._sound = pygame.sndarray.make_sound(y.astype('int16'))
+        self._sound.play(-1)
+        
+        return ''
+
+    @webframework.register_endpoint('/stop_scanning')
+    def stop_scanning(self):
+        if not self._sound is None:
+            self._sound.stop()
+            self._sound = None
+            
+        return ''
+        
+    
+    
+class ScanServer(webframework.APIHTTPServer, ScannerController):
+    def __init__(self, port):
+        ScannerController.__init__(self)
+        
+        server_address = ('', port)
+        webframework.APIHTTPServer.__init__(self, server_address)
+        self.daemon_threads = True
+
+def run(port):
+    import socket
+    
+    externalAddr = socket.gethostbyname(socket.gethostname())
+    server = ScanServer(port=port)
+    
+    try:
+        logger.info('Starting nodeserver on %s:%d' % (externalAddr, port))
+        server.serve_forever()
+    finally:
+        logger.info('Shutting down ...')
+        server.shutdown()
+        server.server_close()
+
+if __name__ == '__main__':
+    port = sys.argv[1]
+    run(int(port))
