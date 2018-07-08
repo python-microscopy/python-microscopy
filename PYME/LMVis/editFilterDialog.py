587580eae64642cb5d810706555824ab6dd89884
Christian Soeller <c.soeller@exeter.ac.uk>
2018-07-08 11:19:59 +0100

    port over David's fix for the edit filter dialog to get sorted pipeline keys

--- PYME/LMVis/editFilterDialog.py
+++ PYME/LMVis/editFilterDialog.py
@@ -33,7 +33,7 @@ class FilterEditDialog(wx.Dialog):
         
         sizer2.Add(wx.StaticText(self, -1, 'Key:'), 0, wx.ALIGN_CENTER_VERTICAL | wx.ALL, 5)
         
-        self.cbKey = wx.ComboBox(self, -1, value=key, choices=possibleKeys, style=wx.CB_DROPDOWN, size=(150, -1))
+        self.cbKey = wx.ComboBox(self, -1, value=key, choices=sorted(possibleKeys), style=wx.CB_DROPDOWN, size=(150, -1))
 
         if not mode == 'new':
             self.cbKey.Enable(False)
