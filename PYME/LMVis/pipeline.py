b97b44749382c9d679339a222bc55f8c5aacaa64
Christian Soeller <c.soeller@exeter.ac.uk>
2018-07-16 16:23:40 +0100

    pipeline - fix duplicate colour names

--- PYME/LMVis/pipeline.py
+++ PYME/LMVis/pipeline.py
@@ -678,15 +678,22 @@ class Pipeline:
 
     def SpecFromMetadata(self):
         labels = self.mdh.getEntry('Sample.Labelling')
-
+        seen_structures = []
+        
         for structure, dye in labels:
+            if structure in seen_structures:
+                strucname = structure + '_1'
+            else:
+                strucname = structure
+            seen_structures.append(structure)
+            
             ratio = dyeRatios.getRatio(dye, self.mdh)
 
             if not ratio is None:
-                self.fluorSpecies[structure] = ratio
-                self.fluorSpeciesDyes[structure] = dye
+                self.fluorSpecies[strucname] = ratio
+                self.fluorSpeciesDyes[strucname] = dye
                 #self.mapping.setMapping('p_%s' % structure, '(1.0/(ColourNorm*2*numpy.pi*fitError_Ag*fitError_Ar))*exp(-(fitResults_Ag - %f*A)**2/(2*fitError_Ag**2) - (fitResults_Ar - %f*A)**2/(2*fitError_Ar**2))' % (ratio, 1-ratio))
-                self.mapping.setMapping('p_%s' % structure, 'exp(-(%f - gFrac)**2/(2*error_gFrac**2))/(error_gFrac*sqrt(2*numpy.pi))' % ratio)
+                self.mapping.setMapping('p_%s' % strucname, 'exp(-(%f - gFrac)**2/(2*error_gFrac**2))/(error_gFrac*sqrt(2*numpy.pi))' % ratio)
                 
 
     def getNeighbourDists(self, forceRetriang = False):
