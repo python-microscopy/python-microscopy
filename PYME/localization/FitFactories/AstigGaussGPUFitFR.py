0ee6978ab23053680aaca69d7018ea7be702e6f2
Andrew Barentine <andrew.barentine@yale.edu>
2018-08-07 14:56:58 -0400

    fix divide by zero before it happens and log the error

--- PYME/localization/FitFactories/AstigGaussGPUFitFR.py
+++ PYME/localization/FitFactories/AstigGaussGPUFitFR.py
@@ -129,7 +129,8 @@ class GaussianFitFactory:
             if varmap == 0:
                 self.varmap = np.ones_like(self.data)
                 logger.error('Variance map not found and read noise defaulted to 0; changing to 1 to avoid x/0.')
-            self.varmap = varmap*np.ones_like(self.data)
+            else:
+                self.varmap = varmap*np.ones_like(self.data)
 
         flatmap = cameraMaps.getFlatfieldMap(self.metadata)
         if not np.isscalar(flatmap):
