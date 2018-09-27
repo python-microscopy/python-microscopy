081575b3e320a42f0f9a325f89c61f273f302da4
David Baddeley <david.baddeley.nz@gmail.com>
2018-09-27 23:55:52 +1200

    added config option to disable countdir

--- PYME/IO/countdir/__init__.py
+++ PYME/IO/countdir/__init__.py
@@ -1,5 +1,5 @@
 from PYME import config
 if config.get('cluster-listing-no-countdir', False):
-    raise RuntimeError('Not using countdir')
+    raise ImportError('Not using countdir')
 
 from countdir import *
