f0ffd4cc539b386507e735a579d88865435d9f26
Christian Soeller <c.soeller@exeter.ac.uk>
2018-10-05 11:13:32 +0100

    recipes base - extractchannel update channelNames md

--- PYME/recipes/base.py
+++ PYME/recipes/base.py
@@ -722,6 +722,11 @@ class ExtractChannel(ModuleBase):
         
         im = ImageStack(chan, titleStub = 'Filtered Image')
         im.mdh.copyEntriesFrom(image.mdh)
+        try:
+            im.mdh['ChannelNames'] = [image.mdh['ChannelNames'][self.channelToExtract]]
+        except KeyError:
+            logger.warn("Error setting channel name")
+
         im.mdh['Parent'] = image.filename
         
         return im
