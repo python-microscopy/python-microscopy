dc5e88402a97235347d96c430f6722788b7c79ca
David Baddeley <david.baddeley.nz@gmail.com>
2019-01-11 05:50:31 +0000

    Removed HT related spooling form piecewise mapping and moved it to HT specific file (present in main branch)

--- PYME/Analysis/piecewiseMapping.py
+++ PYME/Analysis/piecewiseMapping.py
@@ -93,43 +93,6 @@ def framesToTime(fr, events, mdh):
 
     si = sfr.searchsorted(fr, side = 'right')
     return startEvents['Time'][si-1] + (fr - sfr[si-1]) * cycTime
-
-def _spoof_focus_from_metadata(mdh):
-    """
-    Only to be used if events cannot be found, as this is substantially less fool-proof than relying on ProtocolFocus
-    events. Throws a RuntimeWarning if focus cannot be accurately spoofed.
-
-    Parameters
-    ----------
-    mdh : PYME.IO.MetaDataHandler.MDHandlerBase
-
-    Returns
-    -------
-    position : ndarray
-        z positions in microns of each step
-    frames : ndarray
-        frame index at which each step begins
-
-    """
-    try:
-        frames = np.arange(0, mdh['StackSettings.FramesPerStep'] * mdh['StackSettings.NumSteps'] * mdh[
-            'StackSettings.NumCycles'], mdh['StackSettings.FramesPerStep'])
-        position = np.arange(mdh.getOrDefault('Protocol.PiezoStartPos', 0),
-                             mdh.getOrDefault('Protocol.PiezoStartPos', 0) + mdh['StackSettings.NumSteps'] * mdh[
-                                 'StackSettings.StepSize'], mdh['StackSettings.StepSize'])
-        position = np.tile(position, mdh['StackSettings.NumCycles'])
-
-        assert len(position) == len(frames)
-
-    except (KeyError, AttributeError) as e:  # cast error as RuntimeWarning
-        six.reraise(RuntimeWarning,  type(e)('Could not spoof focus position due to incomplete StackSettings metadata'),
-                    sys.exc_info()[2])
-    except AssertionError as e:  # cast error as RuntimeWarning and include more information
-        six.reraise(RuntimeWarning,
-                    type(e)('Error spoofing focus events -  frames: %s, positions %s' % (frames, position)),
-                    sys.exc_info()[2])
-
-    return position, frames
     
 
 class piecewiseMap:
