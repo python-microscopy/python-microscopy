02b5b0a40482ae611a9d13b5a60944ca0151eb31
David Baddeley <david.baddeley.nz@gmail.com>
2019-03-22 15:55:04 +1300

    updates to windows test script

--- run_tests_27.bat
+++ run_tests_27.bat
@@ -1,20 +1,14 @@
-
+@echo on
 set oldenv=%CONDA_DEFAULT_ENV%
 echo %oldenv%
 
 call conda activate pm_test_27
 
-if errorlevel 1 exit 1
-
 "%PYTHON%" setup.py develop
-
-if errorlevel 1 exit 1
 
 cd tests
 pytest -v --html=tests_py27.html --cov=..\PYME --cov-report html:cov_html .
 cd ..
-
-if errorlevel 1 exit 1
 
 conda activate %oldenv%
 
