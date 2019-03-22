b1efdc06fcda428c2c4f5e13d4ec47b80de8aed3
David Baddeley <david.baddeley.nz@gmail.com>
2019-03-22 15:33:17 +1300

    updated netifaces recipe

--- run_tests_27.bat
+++ run_tests_27.bat
@@ -2,12 +2,19 @@
 set oldenv=%CONDA_DEFAULT_ENV%
 echo %oldenv%
 
-conda activate pm_test_27
+call conda activate pm_test_27
 
-python setup.py develop
+if errorlevel 1 exit 1
+
+"%PYTHON%" setup.py develop
+
+if errorlevel 1 exit 1
 
 cd tests
 pytest -v --html=tests_py27.html --cov=..\PYME --cov-report html:cov_html .
+cd ..
+
+if errorlevel 1 exit 1
 
 conda activate %oldenv%
 
