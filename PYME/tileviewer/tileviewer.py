852e70112f81a16291ef8845c5e8a82dc5b3747e
David Baddeley <david.baddeley.nz@gmail.com>
2019-06-05 06:44:30 +1200

    functionality for adding rois to be imaged as actions in PYMEAcquire

--- PYME/tileviewer/tileviewer.py
+++ PYME/tileviewer/tileviewer.py
@@ -21,9 +21,44 @@ env = jinja2.Environment(
 
 class TileServer(object):
     def __init__(self, tile_dir, tile_size=256):
-        self.mdh = MetaDataHandler.load_json(os.path.join(tile_dir, 'metadata.json'))
+        self._set_tile_source(tile_dir)
+        self.roi_locations = []
         
-        self._pyramid =tile_pyramid.ImagePyramid(tile_dir, pyramid_tile_size=self.mdh['Pyramid.TileSize'])
+    def _set_tile_source(self, tile_dir):
+        self.tile_dir = tile_dir
+        self.mdh = MetaDataHandler.load_json(os.path.join(tile_dir, 'metadata.json'))
+        self._pyramid = tile_pyramid.ImagePyramid(tile_dir, pyramid_tile_size=self.mdh['Pyramid.TileSize'])
+        
+    @cherrypy.expose
+    def set_tile_source(self, tile_dir):
+        self._set_tile_source(tile_dir)
+        
+        raise cherrypy.HTTPRedirect('/')
+
+    @cherrypy.expose
+    def set_roi_locations(self, locations_file, tablename='Locations'):
+        from PYME.IO import tabular
+        
+        if locations_file.endswith('.h5'):
+            self.roi_locations = tabular.hdfSource(locations_file, tablename=tablename)
+        # elif locations_file.endswith('.csv'):
+        #     self.roi_locations = tabular.textfileSource(locations_file)
+        raise cherrypy.HTTPRedirect('/')
+    
+    @cherrypy.expose
+    def get_roi_locations(self):
+        if not self.roi_locations is None:
+            return self.roi_locations.toDataFrame().to_json()
+        
+    @cherrypy.expose
+    def add_roi(self, x, y):
+        self.roi_locations.append({'x': float(x), 'y': float(y)})
+        
+        raise cherrypy.HTTPRedirect('/roi_list')
+        
+    @cherrypy.expose
+    def roi_list(self):
+        return env.get_template('roi_list.html').render(roi_list=self.roi_locations)
 
     @cherrypy.expose
     def get_tile(self, layer, x, y, vmin=0, vmax=255):
@@ -45,21 +80,42 @@ class TileServer(object):
         return env.get_template('tileviewer.html').render(tile_size=self.mdh['Pyramid.TileSize'],
                                                           pyramid_width_px=self.mdh['Pyramid.PixelsX'],
                                                           pyramid_height_px=self.mdh['Pyramid.PixelsY'],
-                                                          pyramid_depth=self.mdh['Pyramid.Depth'])
+                                                          pyramid_depth=self.mdh['Pyramid.Depth'],
+                                                          tile_dir = self.tile_dir,
+                                                          )
     
     
     
 if __name__ == '__main__':
     import sys
     tile_server = TileServer(sys.argv[1], tile_size=128)
-
+    
+    from PYME import cluster
+    from PYME import resources
+    
+    static_dir = os.path.join(os.path.split(cluster.__file__)[0], 'clusterUI','static')
+    
+    
     cherrypy.config.update({'server.socket_port': 8979,
-                            'server.socket_host': '0.0.0.0',
-                            'log.screen': False,
+                            'server.socket_host': '127.0.0.1',
+                            #'log.screen': False,
                             'log.access_file': '',
                             'log.error_file': '',
                             'server.thread_pool': 50,
-                            })
+                            #'tools.staticdir.on': True,'tools.staticdir.root': static_dir
+                 })
+
+    conf = {#'/':{
+                            #'tools.staticdir.on': True,'tools.staticdir.root': static_dir
+                 #},
+            '/static':{'tools.staticdir.on': True,
+                       'tools.staticdir.dir': static_dir},
+            '/favicon.ico':
+            {
+                'tools.staticfile.on': True,
+                'tools.staticfile.filename': resources.getIconPath('pymeLogo.png')
+            }
+            }
 
     #logging.getLogger('cherrypy.access').setLevel(logging.ERROR)
 
@@ -69,4 +125,4 @@ if __name__ == '__main__':
     #app = cherrypy.tree.mount(tile_server, '/')
     #app.log.access_log.setLevel(logging.ERROR)
 
-    cherrypy.quickstart(tile_server)
+    cherrypy.quickstart(tile_server, '/', conf)
