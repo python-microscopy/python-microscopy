141edd7388d7381f7cba9b7cb1c467aa59ad733d
David Baddeley <david.baddeley.nz@gmail.com>
2019-06-21 16:36:22 +1200

    manual merge

--- PYME/recipes/processing.py
+++ PYME/recipes/processing.py
@@ -1549,6 +1549,30 @@ class BackgroundSubtractionMovingPercentile(BackgroundSubtractionMovingAverage):
     percentile = Float(0.25)
 
 
+@register_module('Projection')
+class Projection(Filter):
+    """ Project image along an axis
+    
+    TODO - make this more efficient - we currently force the whole stack into memory
+    """
+    
+    kind = Enum(['Mean', 'Max', 'Median', 'Std', 'Min'])
+    axis = Int(2)
+    
+    processFramesIndividually = False
+    
+    def applyFilter(self, data, chanel_num, frame_num, image):
+        if self.kind == 'Mean':
+            return np.mean(data, axis=int(self.axis))
+        if self.kind == 'Max':
+            return np.max(data, axis=int(self.axis))
+        if self.kind == 'Median':
+            return np.median(data, axis=int(self.axis))
+        if self.kind == 'Std':
+            return np.std(data, axis=int(self.axis))
+        if self.kind == 'Min':
+            return np.min(data, axis=int(self.axis))
+
 @register_module('StatisticsByFrame')
 class StatisticsByFrame(ModuleBase):
     """
