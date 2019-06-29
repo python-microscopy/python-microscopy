9f1db39061126d692ea7f2f6e51344ac2cd846d6
Andrew Barentine <andrew.barentine@yale.edu>
2019-06-28 21:00:06 -0400

    preliminary (untuned) PID focus lock

--- PYME/Acquire/Scripts/init_htsms_focus_lock.py
+++ PYME/Acquire/Scripts/init_htsms_focus_lock.py
@@ -20,26 +20,17 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##################
-from PYME.Acquire.ExecTools import joinBGInit, HWNotPresent, init_gui, init_hardware
+from PYME.Acquire.ExecTools import joinBGInit, init_gui, init_hardware
 
 
-from PYME.Acquire.Hardware import fakeShutters
 import time
-import os
-import sys
-
-def GetComputerName():
-    if sys.platform == 'win32':
-        return os.environ['COMPUTERNAME']
-    else:
-        return os.uname()[1]
 
 @init_hardware('Camera')
 def cam(scope):
     from PYME.Acquire.Hardware.uc480 import uCam480
     uCam480.init()
     cam = uCam480.uc480Camera(0)
-    scope.register_camera(cam, 'Focus')
+    scope.register_camera(cam, 'Focus', flipy=True)
 
 #PIFoc
 @init_hardware('PIFoc')
@@ -48,40 +39,35 @@ def pifoc(scope):
     scope.piFoc = offsetPiezoREST.OffsetPiezoClient()
     scope.register_piezo(scope.piFoc, 'z')
 
-@init_gui('Profile')
-def profile(MainFrame,scope):
-    from PYME.ui import fastGraph
-    import numpy as np
+# @init_gui('Profile')
+# def profile(MainFrame,scope):
+#     from PYME.ui import fastGraph
+#     import numpy as np
+#
+#     #xvs = np.arange(scope.frameWrangler.currentFrame.shape[1])
+#
+#     fg = fastGraph.FastGraphPanel(MainFrame, -1, np.arange(10), np.zeros(10))
+#     MainFrame.AddPage(page=fg, select=False, caption='Profile')
+#
+#     def refr_profile(*args, **kwargs):
+#         fg.SetData(np.arange(scope.frameWrangler.currentFrame.shape[1]), scope.frameWrangler.currentFrame.sum(0))
+#
+#     scope.frameWrangler.onFrameGroup.connect(refr_profile)
 
-    #xvs = np.arange(scope.frameWrangler.currentFrame.shape[1])
-
-    fg = fastGraph.FastGraphPanel(MainFrame, -1, np.arange(10), np.zeros(10))
-    MainFrame.AddPage(page=fg, select=False, caption='Profile')
-
-    def refr_profile(*args, **kwargs):
-        fg.SetData(np.arange(scope.frameWrangler.currentFrame.shape[1]), scope.frameWrangler.currentFrame.sum(0))
-
-    scope.frameWrangler.onFrameGroup.connect(refr_profile)
-
-# @init_gui('Drift tracking')
-# def drift_tracking(MainFrame, scope):
-#     from PYME.Acquire.Hardware import driftTracking, driftTrackGUI
-#     scope.dt = driftTracking.correlator(scope, scope.piFoc)
-#     dtp = driftTrackGUI.DriftTrackingControl(MainFrame, scope.dt)
-#     MainFrame.camPanels.append((dtp, 'Focus Lock'))
-#     MainFrame.time1.WantNotification.append(dtp.refresh)
-
-# @init_gui('Focus Keys')
-# def focus_keys(MainFrame, scope):
-#     from PYME.Acquire.Hardware import focusKeys
-#     fk = focusKeys.FocusKeys(MainFrame, scope.piezos[0])
+@init_gui('Focus Lock')
+def focus_lock(MainFrame, scope):
+    from PYME.Acquire.autofocus import FocusLockPID
+    from PYME.Acquire.ui.focus_lock_gui import FocusLockPanel
+    scope.focus_lock = FocusLockPID(scope, scope.piFoc, p=0.1, i=0.001, d=0.0005)
+    scope.focus_lock.register()
+    panel = FocusLockPanel(MainFrame, scope.focus_lock)
+    MainFrame.camPanels.append((panel, 'Focus Lock'))
+    MainFrame.time1.WantNotification.append(panel.refresh)
 
 
 
 #must be here!!!
 joinBGInit() #wait for anyhting which was being done in a separate thread
-
-#scope.SetCamera('A')
 
 time.sleep(.5)
 scope.initDone = True
--- PYME/Acquire/autofocus.py
+++ PYME/Acquire/autofocus.py
@@ -10,6 +10,12 @@
 #
 ##################
 from scipy import ndimage
+from simple_pid import PID
+import numpy as np
+from scipy import optimize
+import logging
+logger = logging.getLogger(__name__)
+import time
 
 class AutoFocus(object):
     def __init__(self, scope, increment=0.5):
@@ -52,3 +58,138 @@ class AutoFocus(object):
         self.incr = incr
         #self.scope.frameWrangler.WantFrameGroupNotification.append(self.tick)
         self.scope.frameWrangler.onFrameGroup.connect(self.OnFrameGroup)
+
+class LineGaussFitter(object):
+    def _model_function(self, parameters, position):
+        """
+        1D gaussian
+        Parameters
+        ----------
+        parameters : tuple
+            fit model parameters
+        distance : ndarray
+            1D position array [pixel]
+        Returns
+        -------
+        model : ndarray
+        """
+        amplitude, center, sigma, bx, b = parameters
+        return amplitude * np.exp(-((position - center) ** 2) / (2 * sigma ** 2)) + bx * position + b
+
+    def _error_function(self, parameters, position, data):
+        """
+        """
+        return data - self._model_function(parameters, position)
+
+    def _calc_guess(self, position, data):
+        offset = data.min()
+        amplitude = data.max() - offset
+        max_ind = np.argmax(data)
+        fwhm = np.sum(data > offset + 0.5 * amplitude)
+        # amplitude, center, sigma, bx, b = parameters
+        return amplitude, position[max_ind], fwhm / 2.355, (data[-1] - data[0]) / (position[-1] - data[0]), offset
+
+    def fit(self, position, data):
+        guess = self._calc_guess(position, data)
+
+        (res, cov_x, infodict, mesg, resCode) = optimize.leastsq(self._error_function, guess, args=(position, data),
+                                                                 full_output=1)
+
+        # estimate uncertainties
+        residuals = infodict['fvec']  # note that fvec is error function evaluation, or (data - model_function)
+        try:
+            # calculate residual variance, a.k.a. reduced chi-squared
+            residual_variance = np.sum(residuals ** 2) / (len(position) - len(guess))
+            # multiply cov by residual variance for estimating parameter variance
+            errors = np.sqrt(np.diag(residual_variance * cov_x))
+        except (
+                TypeError,
+                ValueError) as e:  # cov_x is None for singular matrices -> ~no curvature along at least one dimension
+            print(str(e))
+            errors = -1 * np.ones_like(res)
+
+        return tuple(res.astype('f')), tuple(errors.astype('f'))
+
+class FocusLockPID(PID):
+    def __init__(self, scope, piezo, tolerance=0.025, p=1., i=0.1, d=0.05):
+        """
+
+        Parameters
+        ----------
+        scope: PYME.Acquire.Hardware.microscope.microscope
+        piezo: PYME.Acquire.Hardware.Piezos.offsetPiezoREST.OffsetPiezoClient
+        p: float
+        i: float
+        d: float
+        max_correction_step: float
+            Largest correction [um] to be applied on a single frame
+        """
+        self.scope = scope
+        self.piezo = piezo
+
+        self.tolerance = tolerance
+
+        self._fitter = LineGaussFitter()
+        self.fit_roi_size = 30
+
+        # profile = self.scope.frameWrangler.currentFrame.squeeze().sum(axis=0).astype(float)
+        # self.peak_position = self.find_peak(profile)
+        self.peak_position = 512  # default to half of the camera size
+
+        PID.__init__(self, p, i, d, setpoint=self.peak_position, auto_mode=False)
+
+    @property
+    def lock_enabled(self):
+        return self.auto_mode
+
+    @lock_enabled.setter
+    def lock_enabled(self, enable):
+        self.auto_mode(enable)
+
+    def register(self):
+        self.scope.frameWrangler.onFrameGroup.connect(self.on_frame)
+        self.tracking = True
+
+    def deregister(self):
+        self.scope.frameWrangler.onFrameGroup.disconnect(self.on_frame)
+        self.tracking = False
+
+    def ToggleLock(self, enable=None):
+        if enable is None:
+            self.set_auto_mode(~self.lock_enabled)
+        else:
+            self.set_auto_mode(enable)
+
+    def ChangeSetpoint(self, setpoint=None):
+        if setpoint is None:
+            self.setpoint = self.peak_position
+        else:
+            self.setpoint = setpoint
+
+    @property
+    def fit_roi_size(self):
+        return self._fit_roi_size
+
+    @fit_roi_size.setter
+    def fit_roi_size(self, roi_size):
+        self._fit_roi_size = roi_size
+        self._roi_position = np.arange(roi_size)
+
+    def find_peak(self, profile):
+        crop_start = np.argmax(profile) - int(0.5 * self._fit_roi_size)
+        results, errors = self._fitter.fit(self._roi_position, profile[crop_start:crop_start + self._fit_roi_size])
+        return results[1] + crop_start
+
+    def on_frame(self, **kwargs):
+        t = time.time()
+        logger.debug('grabbing frame')
+        # get focus position
+        profile = self.scope.frameWrangler.currentFrame.squeeze().sum(axis=0).astype(float)
+        self.peak_position = self.find_peak(profile)
+
+        # calculate correction
+        correction = self(self.peak_position)
+        if self.lock_enabled:
+            logger.debug('setpoint: %f, correction :%f, time elapsed: %f' % (self.setpoint, correction, time.time() - t))
+            if abs(correction - self.piezo.GetPos()) > self.tolerance:
+                self.piezo.MoveRel(0, correction)
