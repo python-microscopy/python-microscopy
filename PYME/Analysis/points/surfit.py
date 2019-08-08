b3eb8968934603a47a7f13137640db8ee5eee586
Andrew Barentine <andrew.barentine@yale.edu>
2019-08-08 17:22:01 -0400

    pass fitPos!! and check if we are running in a child process before creating more since this can lead to an assertion error: daemon processes cant have children)

--- PYME/Analysis/points/surfit.py
+++ PYME/Analysis/points/surfit.py
@@ -734,21 +734,18 @@ def fit_quad_surfaces_Pr(data, radius, fitPos=False, NFits=0):
     #nCPUth point. This was done as a simple way of allocating the tasks evenly, but might not be optimal in terms of e.g.
     #cache coherency. The process creation here will be significantly more efficient on *nix platforms which use copy on
     #write forking when compared to windows which will end up copying both the data and kdt structures
-    processes = [multiprocessing.Process(target=fit_quad_surfaces_tr, args=(data, kdt, fnums[i::nCPUs], results, radius))
-                 for i in
-                 range(nCPUs)]
-    
-    #launch all the processes
-    for p in processes:
-        print(p)
-        p.start()
-    
-    #wait for them to complete
-    for p in processes:
-        print(p)
-        p.join()
-    
-    #each process should have written their results into our shared memory array, return this
+    if multiprocessing.current_process().name is 'MainProcess':  # avoid potentially trying to spawn children from daemon
+        processes = [multiprocessing.Process(target=fit_quad_surfaces_tr,
+                                             args=(data, kdt, fnums[i::nCPUs], results, radius, fitPos)) for i in range(nCPUs)]
+        # launch all the processes
+        for p in processes:
+            p.start()
+        # wait for them to complete
+        for p in processes:
+            p.join()
+    else:
+        fit_quad_surfaces_tr(data, kdt, fnums, results, radius, fitPos)
+    # each process should have written their results into our shared memory array, return this
     return results
 
 def reconstruct_quad_surfaces(fits, radius):
