55a657b597ff9194b448d8b7d2a53e09407a427b
Zach Marin <zach.marin@yale.edu>
2019-09-29 20:25:42 -0400

    Tweaks per David's recommendation

--- PYME/experimental/triangle_mesh.py
+++ PYME/experimental/triangle_mesh.py
@@ -51,7 +51,7 @@ VERTEX_DTYPE = np.dtype([('position', '3f4'), ('normal', '3f4'), ('halfedge', 'i
 LOOP_ALPHA_FACTOR = (np.log(13)-np.log(3))/12
 
 class TriangleMesh(object):
-    def __init__(self, vertices, faces, mesh=None, **kwargs):
+    def __init__(self, vertices=None, faces=None, mesh=None, **kwargs):
         """
         Base class for triangle meshes stored using halfedge data structure. 
         Expects STL-like input.
@@ -68,25 +68,22 @@ class TriangleMesh(object):
         """
         if mesh is not None:
             import copy
-            for p, v in vars(mesh).items():
-                if (type(p).__module__ == np.__name__):
-                    setattr(self, p, np.copy(v))
-                # elif isinstance(v, list):
-                #     # We use some lists, this only works in Python 3
-                #     setattr(self, p, v.copy())
-                else:
-                    # Other elements
-                    setattr(self, p, copy.copy(v))
+            
+            self._vertices = np.copy(mesh._vertices)
+            self._vertex_vacancies = copy.copy(mesh._vertex_vacancies)
+
+            self._faces = np.copy(mesh._faces)
+            self._face_vacancies = copy.copy(mesh._face_vacancies)
+
+            self._halfedges = np.copy(mesh._halfedges)
+            self._halfedge_vacancies = copy.copy(mesh._halfedge_vacancies)
         else:
             self._vertices = np.zeros(vertices.shape[0], dtype=VERTEX_DTYPE)
             self._vertices[:] = -1  # initialize everything to -1 to start with
             self._vertices['position'] = vertices
             self._vertex_vacancies = []
-            self._loop_subdivision_flip_edges = []
-            self._loop_subdivision_new_vertices = []
 
             self._faces = None  # Contains a pointer to one halfedge associated with each face
-            self._faces_by_vertex = None  # Representation of faces by triplets of vertices
             self._face_vacancies = []
 
             # Halfedges
@@ -100,22 +97,29 @@ class TriangleMesh(object):
             self._initialize_halfedges(vertices, faces)
             print('done initializing halfedges')
 
-            # Singular edges
-            self._singular_edges = None
-            self._singular_vertices = None
+        # Representation of faces by triplets of vertices
+        self._faces_by_vertex = None
 
-            # Populate the normals
-            self.face_normals
-            self.vertex_normals
+        # loop subdivision vars
+        self._loop_subdivision_flip_edges = []
+        self._loop_subdivision_new_vertices = []
 
-            # Properties we can visualize
-            self.vertex_properties = ['x', 'y', 'z', 'component', 'boundary', 'singular']
+        # Singular edges
+        self._singular_edges = None
+        self._singular_vertices = None
 
-            self.fix_boundary = True  # Hold boundary edges in place
-            self.debug = False  # Print debug statements
+        # Populate the normals
+        self.face_normals
+        self.vertex_normals
 
-            # Is the mesh manifold?
-            self._manifold = None
+        # Properties we can visualize
+        self.vertex_properties = ['x', 'y', 'z', 'component', 'boundary', 'singular']
+
+        self.fix_boundary = True  # Hold boundary edges in place
+        self.debug = False  # Print debug statements
+
+        # Is the mesh manifold?
+        self._manifold = None
 
         # Set debug, fix_boundary, etc.
         for key, value in kwargs.items():
@@ -132,7 +136,7 @@ class TriangleMesh(object):
         return res
 
     def __copy__(self):
-        return type(self)(self.vertices, self.faces, self)
+        return type(self)(mesh=self)
 
     @classmethod
     def from_stl(cls, filename, **kwargs):
@@ -157,7 +161,7 @@ class TriangleMesh(object):
         # Load a PLY from file
         vertices, faces, _ = ply.load_ply(filename)
 
-        return cls(vertices, faces, mesh=None, **kwargs)
+        return cls(vertices, faces, **kwargs)
 
     @classmethod
     def from_np_stl(cls, triangles_stl, **kwargs):
@@ -173,7 +177,7 @@ class TriangleMesh(object):
         faces = faces_raw.reshape(faces_raw.shape[0] // 3, 3, order='F')
 
         print('Data munged to vertices, faces')
-        return cls(vertices, faces, mesh=None, **kwargs)
+        return cls(vertices, faces, **kwargs)
 
     @property
     def x(self):
