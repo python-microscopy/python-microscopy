7e5fa32b92441cbfd5cf01faa4fe75324023af83
Andrew Barentine <andrew.barentine@yale.edu>
2019-11-12 20:22:27 -0500

    clean-up

--- PYME/Acquire/Hardware/arduino/io_slave/io_slave.ino
+++ PYME/Acquire/Hardware/arduino/io_slave/io_slave.ino
@@ -12,6 +12,7 @@ SD  -- Set a digital io pin
 SA  -- Set an analog (PWM) output value 
 SF  -- Set a pin to flash - in this case value is half period in us
 QA  -- Read an analog value (from ADC)
+SS  -- Set servo value
 
 Set commands typically echo something back, which will either need to be read and discarded or flushed. 
  */
@@ -20,20 +21,6 @@ Set commands typically echo something back, which will either need to be read an
 #include <Servo.h>
 
 Servo servos[13];
-/*
-#include <OneWire.h>
-#include <DallasTemperature.h>
-
-// Data wire is plugged into pin 12 on the Arduino
-#define ONE_WIRE_BUS 12
-DeviceAddress thermAddresses[8];
-
-// Setup a oneWire instance to communicate with any OneWire devices
-OneWire oneWire(ONE_WIRE_BUS);
-
-// Pass our oneWire reference to Dallas Temperature. 
-DallasTemperature sensors(&oneWire);
-*/
 
 
 #define INPUT_BUFFER_SIZE 200
@@ -55,19 +42,6 @@ void setup() {
     ; // wait for serial port to connect. Needed for Leonardo only
   }
   
-  // reserve 200 bytes for the inputString:
-  //inputString.reserve(200);
-  /*sensors.begin();
-  sensors.setResolution(12);
-  
-  for (int i = 0;i<max(sensors.getDeviceCount(), 8);i++)
-  {
-    sensors.getAddress(thermAddresses[i], i);
-  }
-  sensors.setWaitForConversion(false);
-  sensors.requestTemperatures();
-  */
-  
   //Attach servos
   servos[9].attach(9);
   servos[10].attach(10);
@@ -79,8 +53,6 @@ void setup() {
 
 void parseCommandArgs(char* command, int *chan, float *val)
 {
-  //command.substring(2).toCharArray(buffer, 200);    
-  //char *p = buffer;
   char *p = command + 2; 
   char *str;
       
@@ -100,45 +72,30 @@ void flash()
 {
   unsigned long micr;
   micr = micros();
-  //unsigned long elapsed;
-  //elapsed = micr - lastMicr;
-  //Serial.println(elapsed);
   for (int i = 0; i < 14; i++)
   {
     if (flashChan[i] && ((micr - lastMicr[i]) > flashPeriod[i]))
     {
       flashStates[i] = !flashStates[i];
-      //Serial.println(flashStates[i]);
       digitalWrite(i, flashStates[i]);
       lastMicr[i] = micr;
     }
   }
-  //lastMicr = micr;
 }  
 
 void loop() {
   flash();
-  // print the string when a newline arrives:
-  /*if (inputLength > 0){
-    sprintf(out, "%d", inputLength);
-    Serial.println(out);
-  }
-    
-  Serial.println(inputBuffer);*/
   if (stringComplete) {
     stringComplete = false;
-    //Serial.println(inputString); 
     
     //querying an analog value
     if (strncmp(inputString, "QA", 2) == 0)
     {
       int chan = int(inputString[2]) - 48;
-      //Serial.println(chan);
       int val = analogRead(chan);
-      //Serial.println(val);
+
       float volts = val*(5.0/1023.0);
       sprintf(out, "%d.%04d", (int)floor(volts), (int)trunc((volts - floor(volts))*10000));
-      //Serial.println(volts);
       Serial.println(out);
     }
     
@@ -153,7 +110,6 @@ void loop() {
       
       pinMode(chan, OUTPUT);
       digitalWrite(chan, int(val));
-      //Serial.println(chan);
       Serial.println(val);
     }
     
@@ -162,14 +118,13 @@ void loop() {
     {
       int chan;
       float val;
-      //int outval;
       
       parseCommandArgs(inputString, &chan, &val);
       
       pinMode(chan, OUTPUT);
       
       analogWrite(chan, int(255.0*val/5.0));
-      //Serial.println(chan);
+
       Serial.println(val);
     }
     
@@ -178,17 +133,11 @@ void loop() {
     {
       int chan;
       float val;
-      //int outval;
       
       parseCommandArgs(inputString, &chan, &val);
-      
-      //pinMode(chan, OUTPUT);
-      
-      //analogWrite(chan, int(255.0*val/5.0));
+
       Serial.println(val);
       servos[chan].write(val);
-      //Serial.println(chan);
-      //Serial.println(val);
     }
     
     //Setting a pin to flash
@@ -209,8 +158,6 @@ void loop() {
         flashChan[chan] = 1;
         flashPeriod[chan] = period;
       } else flashChan[chan] = 0;
-      //Serial.println(chan);
-      //Serial.println(val);
     }
     
     /*
@@ -218,12 +165,9 @@ void loop() {
     if (strncmp(inputString, "QT", 2))
     {
       int chan = int(inputString[2]) - 48;
-      //Serial.println(chan);
+
       float val = sensors.getTempC(thermAddresses[chan]);
-      //Serial.println(val);
-      
-      //sprintf(out, "%d.%02d", (int)floor(val), (int)trunc((val - floor(val))*100));
-      //Serial.println(volts);
+
       Serial.println(val);
     }
     
@@ -231,23 +175,13 @@ void loop() {
     if (strncmp(inputString, "S?", 2))
     {
       int nTemp = sensors.getDeviceCount();
-      //Serial.println(chan);
-      //float val = sensors.getTempC(thermAddresses[chan]);
-      //Serial.println(val);
       
       sprintf(out, "Num Sensors: %d", nTemp);
-      //Serial.println(volts);
       Serial.println(out);
     }*/
     
     // clear the string:
     for (int i = 0; i < INPUT_BUFFER_SIZE; i++) inputString[i] = 0;
-
-    
-    /*if (millis() % 1000)
-    {
-      sensors.requestTemperatures();
-    }*/
   }
   
   serialPoll();
