d8892a381048c1d51558aa835c6cc26126ef8468
David Baddeley <david.baddeley.nz@gmail.com>
2019-12-14 04:18:52 +1300

    fix tiling so that coordinates are OK regardless of which multiview ROI is used for the overview

--- PYME/Acquire/ui/actionUI.py
+++ PYME/Acquire/ui/actionUI.py
@@ -193,11 +193,14 @@ class ActionPanel(wx.Panel):
         
         # coordinates are for the centre of ROI, and are referenced to the 0,0 pixel of the camera,
         # correct this for a custom ROI.
-        # TODO - does this play nicely with Andrew's MultiView camera?
-        x0, y0, x1, y1 = self.scope.state['Camera.ROI']
+        x0, y0, _, _ = self.scope.state['Camera.ROI']
         
         if isinstance(self.scope.cam, MultiviewCameraMixin):
             # fix multiview crazyness
+            if self.scope.cam.multiview_enabled:
+                #always use the 0th ROI for determining relative position, regardless of which ROIs are active
+                x0, y0 = self.scope.cam.view_origins[0]
+            
             roi_offset_x = self.scope.GetPixelSize()[0] * (x0 + 0.5 * self.scope.cam.size_x)
             roi_offset_y = self.scope.GetPixelSize()[1] * (y0 + 0.5 * self.scope.cam.size_y)
         else:
