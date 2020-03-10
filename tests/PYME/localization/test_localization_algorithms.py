fee64f759f13099c77238a67600cedf8ea5b62cb
zacsimile <zach.marin@yale.edu>
2020-03-10 16:19:35 -0400

    Dumbell 3d testing

--- tests/PYME/localization/test_localization_algorithms.py
+++ tests/PYME/localization/test_localization_algorithms.py
@@ -40,6 +40,16 @@ def test_LatGaussFitFR():
     
     assert errors_over_pred_IQR < 2.5
 
+def test_Dumbell3DFitR():
+    from PYME.localization.Test import fitTestJigWC as fitTestJig
+    
+    tj = fitTestJig.fitTestJig.fromMDFile(os.path.join(TESTPATH, 'dumbell_3d.md'))
+    tj.runTests(nTests=100)
+    
+    errors_over_pred_IQR = fitTestJig.IQR((tj.error('x0') / tj.res['fitError']['x0']))
+    print(errors_over_pred_IQR)
+    
+    assert errors_over_pred_IQR < 2.5
 
 def test_InterpFitR_astigmatism():
     """Test the 3D interpolated fit by fitting some randomly generated events. The pass condition here is fairly
