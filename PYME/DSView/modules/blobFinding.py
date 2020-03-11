ffde8a4251f8bde2a4c1f025d154725e3da76336
zacsimile <zachcm@gmail.com>
2020-03-11 16:45:47 -0400

    Call slice from blobFinding

--- PYME/DSView/modules/blobFinding.py
+++ PYME/DSView/modules/blobFinding.py
@@ -162,7 +162,7 @@ class blobFinder:
         #and identify objects ...
         if self.chMethod.GetSelection() == 1: #don't detect objects in poisson noise
             fudgeFactor = 1 #to account for the fact that the blurring etc... in ofind doesn't preserve intensities - at the moment completely arbitrary so a threshold setting of 1 results in reasonable detection.
-            threshold =  (numpy.sqrt(self.image.mdh.Camera.ReadNoise**2 + numpy.maximum(self.image.mdh.Camera.ElectronsPerCount*(self.image.mdh.Camera.NoiseFactor**2)*(self.image.data.astype('f') - self.image.mdh.Camera.ADOffset)*self.image.mdh.Camera.TrueEMGain, 1))/self.image.mdh.Camera.ElectronsPerCount)*fudgeFactor*threshold
+            threshold =  (numpy.sqrt(self.image.mdh.Camera.ReadNoise**2 + numpy.maximum(self.image.mdh.Camera.ElectronsPerCount*(self.image.mdh.Camera.NoiseFactor**2)*(self.image.data[:,:,:, chnum] - self.image.mdh.Camera.ADOffset)*self.image.mdh.Camera.TrueEMGain, 1))/self.image.mdh.Camera.ElectronsPerCount)*fudgeFactor*threshold
             self.ofd.FindObjects(threshold, 0)
         elif self.chMethod.GetSelection() == 2:
             bs = float(self.tBlurSize.GetValue())
