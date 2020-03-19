76561095081040b1ff34b72e505ffcd413422328
barentine <andrew.barentine@gmail.com>
2020-03-19 18:50:41 -0400

    fix distutils install to do the cythonizing

--- PYME/Analysis/points/traveling_salesperson/setup.py
+++ PYME/Analysis/points/traveling_salesperson/setup.py
@@ -1,13 +1,36 @@
 
+import sys
+import os
 
-def configuration(parent_package = '', top_path = None):
+if sys.platform == 'darwin'  :  # MacOS
+    linkArgs = []
+else:
+    linkArgs = ['-static-libgcc']
+
+# windows VC++ has really shocking c standard support so we need to include
+# custom stdint.h and intypes.h files from https://code.google.com/archive/p/msinttypes
+# print os.environ.get('CC', 'foo')
+if sys.platform == 'win32' and not os.environ.get('CC', '') == 'mingw':
+    extra_include_dirs = ['win_incl']
+else:
+    extra_include_dirs = []
+
+from Cython.Build import cythonize
+
+def configuration(parent_package='', top_path=None):
+
+    from numpy.distutils.core import Extension
     from numpy.distutils.misc_util import Configuration, get_numpy_include_dirs
-    config = Configuration('astigmatism', parent_package, top_path)
 
-    config.add_extension('two_opt_utils',
-        sources=['two_opt_utils.c'],
-        include_dirs = [get_numpy_include_dirs()])
+    cur_dir = os.path.dirname(__file__)
 
+    ext = Extension(name='.'.join([parent_package, 'two_opt_utils']),
+                    sources=[os.path.join(cur_dir, 'two_opt_utils.pyx')],
+                    include_dirs= get_numpy_include_dirs() + extra_include_dirs,
+                    extra_compile_args=['-O3', '-fno-exceptions', '-ffast-math', '-march=native', '-mtune=native'],
+                    extra_link_args=linkArgs)
+
+    config = Configuration('traveling_salesperson', parent_package, top_path, ext_modules=cythonize([ext]))
     return config
 
 if __name__ == '__main__':
