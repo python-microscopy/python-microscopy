5d82011fad6296480a7b6e307e6ebb6705d92a37
David Baddeley <19475296+David-Baddeley@users.noreply.github.com>
2020-04-01 14:20:31 +1300

    Quick view changes
    - default view has separators between inputs, parameters, outputs
    - only have an OK button, as traits don't roll-back when you click cancel (i.e. clicking cancel is the same as clicking OK)
    - added pipeline view

--- PYME/recipes/localisations.py
+++ PYME/recipes/localisations.py
@@ -425,6 +425,19 @@ class DBSCANClustering(ModuleBase):
     @property
     def hide_in_overview(self):
         return ['columns']
+    
+    @property
+    def pipeline_view(self):
+        from traitsui.api import View, Item, TextEditor
+        from PYME.ui.custom_traits_editors import CBEditor
+
+        return View(Item('columns', editor=TextEditor(auto_set=False, enter_set=True, evaluate=ListStr)),
+                    Item('searchRadius'),
+                    Item('minClumpSize'),
+                    Item('multithreaded'),
+                    Item('numberOfJobs'),
+                    Item('clumpColumnName'),
+                    )
 
     @property
     def default_view(self):
@@ -432,14 +445,16 @@ class DBSCANClustering(ModuleBase):
         from PYME.ui.custom_traits_editors import CBEditor
 
         return View(Item('inputName', editor=CBEditor(choices=self._namespace_keys)),
+                    Item('_'),
                     Item('columns', editor=TextEditor(auto_set=False, enter_set=True, evaluate=ListStr)),
                     Item('searchRadius'),
                     Item('minClumpSize'),
                     Item('multithreaded'),
                     Item('numberOfJobs'),
                     Item('clumpColumnName'),
+                    Item('_'),
                     Item('outputName'), 
-                    buttons=['OK', 'Cancel'])
+                    buttons=['OK'])
 
 #TODO - this is very specialized and probably doesn't belong here - at least not in this form
 @register_module('ClusterCountVsImagingTime')
