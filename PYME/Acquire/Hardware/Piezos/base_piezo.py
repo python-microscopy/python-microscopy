3f443d0b1c543bbd732035fa909a090cab81876c
David Baddeley <19475296+David-Baddeley@users.noreply.github.com>
2020-05-28 12:18:53 +1200

    Update base_piezo.py

--- PYME/Acquire/Hardware/Piezos/base_piezo.py
+++ PYME/Acquire/Hardware/Piezos/base_piezo.py
@@ -2,14 +2,16 @@ class PiezoBase(object):
     """
     Base class for all stages
 
-    Attributes
-    ----------
+    Attributes (can be over-ridden in derived classes)
+    --------------------------------------------------
     units_um: float
         multiplier to get from piezo units to micrometers. For a stage internally using millimeters, this would be
-        1000, while a stage internally using nanometers would use 0.001, etc..
+        1000, while a stage internally using nanometers would use 0.001, etc ... Exists for compatibility purposes for
+        old stages which had variable units. New stage implementations should try and stick to um and leave this as 1.
     gui_description: str
-        [optional/deprecated] abbreviated description of stage axis, e.g. 'x-piezo'. This is typically handled
-        downstream in PYME.Acquire.microscope.register_piezo
+        Template for a display name. Used together with the `axis` argument to PYME.Acquire.microscope.register_piezo to 
+        generate a suitable display name for the stage. Can be over-ridden for asthetic purposes in e.g. stepper motors which
+        use PiezoBase as a base, so their controls do not say piezo.
 
     """
     gui_description = '%s-piezo'
