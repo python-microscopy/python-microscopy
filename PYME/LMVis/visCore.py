948b0b8a8a6787305ae0259ff776d07f9460e696
barentine <andrew.barentine@gmail.com>
2020-07-21 22:00:01 -0400

    select Localizations datasource - until we move pipeline mappings into the pipeline recipe this will be the only datasource which actually updates

--- PYME/LMVis/visCore.py
+++ PYME/LMVis/visCore.py
@@ -668,7 +668,10 @@ class VisGUICore(object):
         elif os.path.splitext(filename)[1] == '.hdf':
             pass
         elif filename.endswith('/live'):
-            pass
+            # select the live-updating datasource once we're done loading.
+            # FIXME - once pipeline mappings are done in the pipeline.recipe
+            # we can replace this with `pass` as MappingFilters etc. will then be rebuilt
+            wx.CallAfter(self.pipeline.selectDataSource, 'Localizations')
         elif os.path.splitext(filename)[1] == '.mat':
             from PYME.LMVis import importTextDialog
             from scipy.io import loadmat
