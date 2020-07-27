fix units - roi_locations from ImageRecipes typically come out in micometers, while tabulars are typically nanometers in PYME. Default to micrometers, which is the unit used by stages

PYME/recipes/acquisition.py
@@ -18,6 +18,9 @@ class QueueAcquisitions(OutputModule):
     input_positions : Input
         PYME.IO.tabular containing 'x' and 'y' coordinates in units of 
         nanometers
+    position_units : CStr
+        Units the `input_positions` are in. Should be 'nm' for nanometers or 
+        'um' for micrometers. 'um' by default.
     action_server_url : CStr
         URL of the microscope-side action server process.
     spool_settings : DictStrAny
@@ -62,6 +65,7 @@ class QueueAcquisitions(OutputModule):
         microscope-side server. Can be set to zero for ~no throttling.
     """
     input_positions = Input('input')
+    position_units = CStr('um')
     action_server_url = CStr('http://127.0.0.1:9393')
     spool_settings = DictStrAny()
     lifo = Bool(True)
@@ -84,9 +88,16 @@ class QueueAcquisitions(OutputModule):
             resolved.
         """
         
+        if self.position_units not in ['nm', 'um']:
+            raise ValueError('input positions must be "nm" or "um"')
+
         positions = np.stack((namespace[self.input_positions]['x'], 
                               namespace[self.input_positions]['y']), 
-                              axis=1) / 1e3  # (N, 2), nm -> um
+                              axis=1) # (N, 2)
+        
+        if self.position_units == 'nm':
+            # PYME stages use micrometers
+            positions = positions / 1e3  # nm -> um
 
         if len(self.nice_range) == 2:
             nices = np.linspace(self.nice_range[0], self.nice_range[1], 