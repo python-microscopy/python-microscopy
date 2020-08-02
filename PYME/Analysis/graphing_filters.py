e2e577cca1308b3eab41e6d572ae1b00fc09e88c
zacsimile <zachcm@gmail.com>
2020-08-02 13:48:45 -0400

    Ensure slicing is integer-based

--- PYME/Analysis/graphing_filters.py
+++ PYME/Analysis/graphing_filters.py
@@ -235,7 +235,7 @@ def movieplot2(clump, image):
             contours = None
         
         for i in range(clump.nEvents):
-            k = np.floor(i / 10)
+            k = int(np.floor(i / 10))
             l = i % 10
             
             y_0 = l * (roiSize + 2)
@@ -255,7 +255,7 @@ def movieplot2(clump, image):
                 else:
                     scMax = img.max()
                 
-                #print x_0, y_0, img.shape, img_out.shape, i
+                print(x_0, y_0, img.shape, img_out.shape, i)
                 
                 img_out[x_0:(x_0 + roiSize), y_0:(y_0 + roiSize), 0] = np.clip(255. * img.T / scMax, 0, 255).astype(
                     'uint8')
