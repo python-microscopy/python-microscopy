cfc26eb0e5fba2aabee789e9090912e16e9017a2
Barentine <andrew.barentine@yale.edu>
2020-08-10 16:03:24 -0400

    round early in averageframesbyzstep so we don't later have multiple frames on the same step

--- PYME/recipes/processing.py
+++ PYME/recipes/processing.py
@@ -1747,6 +1747,9 @@ class AverageFramesByZStep(ModuleBase):
         else:
             #z values are provided as input
             z_vals = namespace[self.input_zvals][self.z_column_name]
+        
+        # later we will mash z with %3.3f, round here so we don't duplicate steps
+        z_vals = np.round(z_vals, decimals=3)
 
         # make sure everything is sorted. We'll carry the args to sort, rather than creating another full array
         frames_z_sorted = np.argsort(z_vals)
