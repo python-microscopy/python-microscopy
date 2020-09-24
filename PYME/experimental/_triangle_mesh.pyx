f6fe3ee99593af69d897bfd9d2a040c5a447dbd8
zacsimile <zachcm@gmail.com>
2020-09-24 12:30:54 -0400

    Fix typo

--- PYME/experimental/_triangle_mesh.pyx
+++ PYME/experimental/_triangle_mesh.pyx
@@ -2075,7 +2075,7 @@ cdef class TriangleMesh(TrianglesBase):
             # TODO - move this to a helper function
             self._vertices['locally_manifold'] = 1
             self._vertices['locally_manifold'][self._halfedges['vertex'][self._halfedges['twin'] == -1]] = 0
-            self._vertices['locally_manifold'][-1] = (self._halfedges['vertex'] != -1) and (self._halfedges['twin'][-1] == -1)
+            self._vertices['locally_manifold'][-1] = (self._halfedges['vertex'][-1] != -1) and (self._halfedges['twin'][-1] == -1)
             
             for i in range(n_halfedges):
                 if (self._chalfedges[i].vertex != -1) and (self._chalfedges[i].length < collapse_threshold):
@@ -2089,7 +2089,7 @@ cdef class TriangleMesh(TrianglesBase):
             # TODO - move this to a helper function / make collapse update this so we don't need to recompute
             self._vertices['locally_manifold'] = 1
             self._vertices['locally_manifold'][self._halfedges['vertex'][self._halfedges['twin'] == -1]] = 0
-            self._vertices['locally_manifold'][-1] = (self._halfedges['vertex'] != -1) and (self._halfedges['twin'][-1] == -1)
+            self._vertices['locally_manifold'][-1] = (self._halfedges['vertex'][-1] != -1) and (self._halfedges['twin'][-1] == -1)
             
             self.regularize()
 
