808e4702d90fe08a72c8955e0e5da13c0f8be5da
David Baddeley <david.baddeley.nz@gmail.com>
2020-10-20 12:39:37 +1300

    initial rule factory structure

---
+++ PYME/cluster/rules.py
@@ -0,0 +1,263 @@
+import six
+import json
+import threading
+import logging
+logger = logging.getLogger(__name__)
+
+        
+class RuleFactory(object):
+    def __init__(self, on_completion=None, **kwargs):
+        """
+        Create a new rule factory. Sub-classed for specific rule types
+        
+        
+        Parameters
+        ----------
+        on_completion : RuleFactory instance
+            A rule to run after this one has completed (also setable using the `.chain()` method)
+             
+        kwargs : any additional arguments (ignored in base class)
+        
+        """
+        self._on_completion = on_completion
+        
+    def get_rule(self, context):
+        """
+        Populate a rule using series specific info from context
+        
+        Parameters
+        ----------
+        context : dict
+            a dictionary containing series specific info to populate into the task template
+
+        Returns
+        -------
+        
+        a rule suitable for submitting to the ruleserver `/add_integer_id_rule` endpoint
+
+        """
+        rule = {'template': self.task_template(context)}
+        
+        if self._on_completion:
+            rule['on_completion'] = self._on_completion.get_rule(context)
+        
+        return rule
+    
+    def task_template(self, context):
+        """
+        Populate the task template for a given rule type. Should be implemented in derived classes.
+        
+        Parameters
+        ----------
+        context : dict
+            Series specific context information to use in the template
+
+        Returns
+        -------
+        
+        The task template as a string.
+
+        """
+        raise NotImplementedError('This method should be over-ridden in a derived class')
+    
+    def chain(self, on_completion):
+        """
+        Chain a rule (as an alternative to passing on_completion to the rule constructor, allows us to create rule chains
+        from front to back rather than back to front
+        
+        Parameters
+        ----------
+        
+        on_completion : RuleFactory instance
+             The rule to run after this one has completed
+        """
+        assert(isinstance(on_completion, RuleFactory))
+        self._on_completion = on_completion
+        
+        
+class RecipeRuleFactory(RuleFactory):
+    def __init__(self, recipe=None, recipeURI=None, output_dir=None, **kwargs):
+        """
+        Create a recipe rule factory
+        
+        Parameters
+        ----------
+        recipe : str or recipes.modules.ModuleCollection
+            The recipe as YAML text or as a recipe instance (alternatively provide recipeURL)
+        recipeURI : str
+            A cluster URI for the recipe text (if `recipe` not provided directly)
+        output_dir : str
+            The directory to put the recipe output TODO: should this be templated based on context?
+        kwargs : any additional args to get passed to base class - e.g. on_completion
+        
+        TODO - support for templated recipes? Subclass?
+        """
+        RuleFactory.__init__(**kwargs)
+
+        if recipe:
+            if isinstance(recipe, six.string_types):
+                self.recipe_text = recipe
+            else:
+                self.recipe_text = recipe.toYAML()
+    
+            self.recipeURI = None
+        else:
+            if recipeURI is None:
+                raise ValueError('recipeURI must be defined if no recipe given')
+            else:
+                self.recipeURI = recipeURI
+
+        self.output_dir = output_dir
+        
+    def get_rule(self, context):
+        #over-ride here because we need to add input info
+        rule = RuleFactory.get_rule(self, context)
+        rule['inputsByTask'] = inputs_by_task
+        
+        return rule
+    
+    def task_template(self, context):
+        task = '''{"id": "{{ruleID}}~{{taskID}}",
+                      "type": "recipe",
+                      "inputs" : {{taskInputs}},
+                      %s,
+                      %s
+                      }'''
+    
+        if self.output_dir is None:
+            output_dir_n = ''
+        else:
+            output_dir_n = '"output_dir": "%s",' % self.output_dir
+    
+        if self.recipeURI:
+            task = task % ('"taskdefRef" : "%s"' % self.recipeURI, output_dir_n)
+        else:
+            task = task % ('"taskdef" : {"recipe": "%s"}' % self.recipe_text, output_dir_n)
+    
+        return task
+    
+class LocalisationRuleFactory(RuleFactory):
+    def __init__(self, **kwargs):
+        RuleFactory.__init__(self, **kwargs)
+        
+    def task_template(self, context):
+        tt = {'id': '{{ruleID}}~{{taskID}}',
+              'type': 'localization',
+              'taskdef': {'frameIndex': '{{taskID}}', 'metadata': context['resultsMdURI']},
+              'inputs': {'frames': context['dataSourceID']},
+              'outputs': {'fitResults': context['resultsURI'] + '/FitResults',
+                          'driftResults': context['resultsURI'] + '/DriftResults'}
+              }
+        self._task_template = json.dumps(tt)
+
+
+
+import time
+class RulePusher(object):
+    def __init__(self, rule_factory, **context):
+        self._rule_factory = rule_factory
+        self._context = context
+        self._rule = rule_factory.get_rule(context)
+        
+        self._ruleID = None
+        self.taskQueueURI = self._getTaskQueueURI()
+        
+        self._current_task_num = 0
+        self._complete = False
+    
+        #post our rule
+        self._post_rule()
+
+        self.doPoll = True
+        self.pollT = threading.Thread(target=self._poll_loop())
+        self.pollT.start()
+        
+    @classmethod
+    def _getTaskQueueURI(cls, n_retries=2):
+        """Discover the distributors using zeroconf and choose one"""
+        from PYME.misc import hybrid_ns
+        import socket
+        import random
+        import time
+        from PYME.misc.computerName import GetComputerName
+        compName = GetComputerName()
+    
+        ns = hybrid_ns.getNS('_pyme-taskdist')
+    
+        queueURLs = {}
+    
+        def _search():
+            for name, info in ns.get_advertised_services():
+                if name.startswith('PYMERuleServer'):
+                    print(info, info.address)
+                    queueURLs[name] = 'http://%s:%d' % (socket.inet_ntoa(info.address), info.port)
+    
+        _search()
+        while not queueURLs and (n_retries > 0):
+            logging.info('could not find a rule server, waiting 5s and trying again')
+            time.sleep(5)
+            n_retries -= 1
+            _search()
+    
+        try:
+            #try to grab the distributor on the local computer
+            local_queues = [q for q in queueURLs if compName in q]
+            logger.debug('local_queues: %s' % local_queues)
+            return queueURLs[local_queues[0]]
+        except (KeyError, IndexError):
+            #if there is no local distributor, choose one at random
+            logger.info('no local rule server, choosing one at random')
+            return random.choice(list(queueURLs.values()))
+
+    def _post_rule(self, timeout=3600, max_tasks=1e6, release_start=None, release_end=None):
+        """ wrapper around add_integer_rule api endpoint"""
+        from PYME.IO import clusterIO
+    
+        s = clusterIO._getSession(self.taskQueueURI)
+        if release_start is None:
+            cmd = '%s/add_integer_id_rule?timeout=%d&max_tasks=%d' % (self.taskQueueURI, timeout, max_tasks)
+        else:
+            # TODO - can we get rid of this special casing?
+            cmd = '%s/add_integer_id_rule?timeout=%d&max_tasks=%d&release_start=%d&release_end=%d' % (self.taskQueueURI, timeout, max_tasks, release_start, release_end)
+            
+        r = s.post(cmd,
+                   data=json.dumps(self._rule),
+                   headers={'Content-Type': 'application/json'})
+    
+        if r.status_code == 200:
+            resp = r.json()
+            self._ruleID = resp['ruleID']
+            logger.debug('Successfully created rule')
+        else:
+            logger.error('Failed creating rule with status code: %d' % r.status_code)
+            
+    def _release_tasks(self, release_start, release_end):
+        """ Thin wrapper around release_rule_tasks api endpoint"""
+        from PYME.IO import clusterIO
+        s = clusterIO._getSession(self.taskQueueURI)
+        r = s.get('%s/release_rule_tasks?ruleID=%s&release_start=%d&release_end=%d' % (
+        self.taskQueueURI, self._ruleID, release_start, release_start),
+                  data='',
+                  headers={'Content-Type': 'application/json'})
+    
+        if r.status_code == 200 and r.json()['ok']:
+            logging.debug('Successfully released tasks')
+        else:
+            logging.error('Failed on releasing tasks with status code: %d' % r.status_code)
+        
+            
+    def _poll_loop(self):
+        logging.debug('task pusher poll loop started')
+        #wait until clusterIO caches clear to avoid replicating the results file.
+        time.sleep(1.5)
+    
+        while (self.doPoll == True):
+            framesOutstanding = self.fileTasksForFrames()
+            if self.ds.is_complete and not (framesOutstanding > 0):
+                logging.debug('all tasks pushed, ending loop.')
+                self.doPoll = False
+            else:
+                time.sleep(1)
+
+    def cleanup(self):
+        self.doPoll = False
