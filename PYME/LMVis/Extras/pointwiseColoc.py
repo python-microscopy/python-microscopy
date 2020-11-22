8b40c7b5c6ea4b8b795a3d067f8c1bcfd86c3113
zacsimile <zachcm@gmail.com>
2020-11-22 16:56:30 -0500

    The theoretically correct way of handling the showmodal cases

--- PYME/LMVis/Extras/pointwiseColoc.py
+++ PYME/LMVis/Extras/pointwiseColoc.py
@@ -18,25 +18,32 @@ class PointwiseColocaliser:
         visFr.AddMenuItem('Analysis', "Pointwise Colocalisation", self.OnPointwiseColoc)
         
     def OnPointwiseColoc(self, event):
+        import wx
         #from PYME import mProfile
         #mProfile.profileOn(['distColoc.py'])
         from PYME.Analysis.Colocalisation import distColoc
         from PYME.DSView.modules.coloc import ColocSettingsDialog
 
-        chans = self.visFr.pipeline.colourFilter.getColourChans()[::1]
+        chans = self.visFr.pipeline.colourFilter.getColourChans()
+
+        if len(chans) == 0:
+            md = wx.MessageDialog(self.visFr, 'Not enough colour channels', 'Pointwise colocalization requires 2 colour channels', wx.OK)
+            md.ShowModal()
+            return
 
         dlg = ColocSettingsDialog(self.visFr, names=chans, show_bins=False)
-        dlg.ShowModal()
+        
+        if dlg.ShowModal() == wx.ID_OK:
 
-        selected_chans = [chans[int(x)] for x in dlg.GetChans()]  # re-index into chans b/c calcDistCorr expects full channel names
+            selected_chans = [chans[int(x)] for x in dlg.GetChans()]  # re-index into chans b/c calcDistCorr expects full channel names
+
+            #A vs B
+            distColoc.calcDistCorr(self.visFr.pipeline.colourFilter, *selected_chans)
+            #B vs A
+            #distColoc.calcDistCorr(self.visFr.colourFilter, *(self.visFr.colourFilter.getColourChans()[::-1]))
+            #mProfile.report()
+
         dlg.Destroy()
-
-        #A vs B
-        distColoc.calcDistCorr(self.visFr.pipeline.colourFilter, *selected_chans)
-        #B vs A
-        #distColoc.calcDistCorr(self.visFr.colourFilter, *(self.visFr.colourFilter.getColourChans()[::-1]))
-        #mProfile.report()
-
 
 
 def Plug(visFr):
