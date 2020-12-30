5fc005e96076fa822428ff3c58e66ce315f96229
barentine <andrew.barentine@gmail.com>
2020-12-30 14:45:08 -0500

    preliminary rule display page 'functional'

--- PYME/Acquire/ui/rules.py
+++ PYME/Acquire/ui/rules.py
@@ -8,11 +8,16 @@ import queue
 import os
 import posixpath
 import logging
-from PYME.contrib import dispatch
+from PYME.contrib import dispatch, wxPlotPanel
 from PYME.recipes.traits import HasTraits, Enum, Float, CStr
+from PYME.Acquire.protocol import get_protocol_list
+import textwrap
+import numpy as np
+import matplotlib.pyplot as plt
 
 logger = logging.getLogger(__name__)
 
+POST_CHOICES = ['off', 'spool start', 'spool stop']
 
 class RuleTile(HasTraits):
     task_timeout = Float(60 * 10)
@@ -43,7 +48,7 @@ def get_rule_tile(rule_factory_class):
 
 
 class RuleChain(HasTraits):
-    post_on = Enum(['off', 'spool start', 'spool stop'])
+    post_on = Enum(POST_CHOICES)
     protocol = CStr('')
     
     def __init__(self, rule_factories=None, *args, **kwargs):
@@ -157,6 +162,70 @@ class ProtocolRuleFactoryListCtrl(wx.ListCtrl):
     def get_next_selected(self, current):
         return self.GetNextItem(current, wx.LIST_NEXT_ALL, wx.LIST_STATE_SELECTED)
 
+
+class RulePlotPanel(wxPlotPanel.PlotPanel):
+    def __init__(self, parent, protocol_rules, **kwargs):
+        self.protocol_rules = protocol_rules
+        self.parent = parent
+        wxPlotPanel.PlotPanel.__init__(self, parent, **kwargs)
+        self.figure.canvas.mpl_connect('pick_event', self.parent.OnPick)
+
+    def draw(self):
+        if not self.IsShownOnScreen():
+            return
+            
+        if not hasattr( self, 'ax' ):
+            self.ax = self.figure.add_axes([0, 0, 1, 1])
+
+        self.ax.cla()
+
+        rule_factories = self.parent.rule_chain.rule_factories
+
+        nodes_x = np.linspace(0, 1, len(rule_factories) + 2)[1:-1]
+        nodes_y = np.ones_like(nodes_x)
+
+        
+        axis_width = self.ax.get_window_extent().width
+        n_cols = max([1] + nodes_x.tolist())
+        pix_per_col = axis_width / n_cols
+        
+        font_size = max(6, min(10, 10 * pix_per_col / 100))
+        
+        TW = textwrap.TextWrapper(width=max(int(1.8 * pix_per_col / font_size), 10),
+                                  subsequent_indent='  ')
+        TW2 = textwrap.TextWrapper(width=max(int(1.3 * pix_per_col / font_size), 10),
+                                   subsequent_indent='  ')
+    
+        cols = {}
+
+        # plot the connecting lines
+        for xv, yv in zip(nodes_x, nodes_y):
+            self.ax.plot(xv, yv, lw=2)
+                
+        #plot the boxes and the labels
+        for ind in range(len(rule_factories)):
+            # draw a box
+            s = rule_factories[ind]._type
+            fc = [.8,.8, 1]
+                
+            rect = plt.Rectangle([nodes_x[ind], nodes_y[ind]], 1, .5, ec='k', lw=2, fc=fc, picker=True)
+            rect._data = s
+            self.ax.add_patch(rect)
+            
+            s = TW2.wrap(s)
+            if len(s) == 1:
+                self.ax.text(nodes_x[ind] + .05, nodes_y[ind] + .18 , s[0], size=font_size, weight='bold')
+            else:
+                self.ax.text(nodes_x[ind] + .05, nodes_y[ind] + .18 - .05*(len(s) - 1) , '\n'.join(s), size=font_size, weight='bold')
+        
+        self.ax.set_ylim(0, 2)
+        self.ax.set_xlim(0, 1)
+        
+        self.ax.axis('off')
+        self.ax.grid()
+        
+        self.canvas.draw()
+
 class ChainedAnalysisPage(wx.Panel):
     def __init__(self, parent, protocol_rules, recipe_manager, 
                  spool_controller, default_pairings=None):
@@ -181,6 +250,186 @@ class ChainedAnalysisPage(wx.Panel):
         self._selected_protocol = list(protocol_rules.keys())[0]
         self._recipe_manager = recipe_manager
         self._spool_controller = spool_controller
+
+        hsizer = wx.BoxSizer(wx.HORIZONTAL)
+        vsizer = wx.BoxSizer(wx.VERTICAL)
+
+        # associated protocol choicebox (default / others on top, then rest)
+        hsizer.Add(wx.StaticText(self, -1, 'Associated Protocol:'), 0, 
+                   wx.LEFT|wx.RIGHT, 5)
+        self.c_protocol = wx.Choice(self, -1, choices=get_protocol_list())
+        self.c_protocol.SetSelection(0)
+        self.c_protocol.Bind(wx.EVT_CHOICE, self.OnProtocolChoice)
+        hsizer.Add(self.c_protocol, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 2)
+
+        # post on choicebox
+        hsizer.Add(wx.StaticText(self, -1, 'Post On:'), 0, 
+                   wx.LEFT|wx.RIGHT, 5)
+        self.c_post = wx.Choice(self, -1, choices=POST_CHOICES)
+        self.c_post.SetSelection(0)
+        self.c_post.Bind(wx.EVT_CHOICE, self.OnPostChoice)
+        hsizer.Add(self.c_post, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 2)
+
+        vsizer.Add(hsizer, 0)
+
+        # rule plot
+        self.rule_plot = RulePlotPanel(self, self._protocol_rules, 
+                                       size=(-1, 400))
+        vsizer.Add(self.rule_plot, 1, wx.ALL|wx.EXPAND, 5)
+        
+        hsizer = wx.BoxSizer(wx.HORIZONTAL)
+        
+        self.b_clear = wx.Button(self, -1, 'Clear Chain')
+        hsizer.Add(self.b_clear, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 2)
+        self.b_clear.Bind(wx.EVT_BUTTON, self.OnClear)
+
+        self.b_add_recipe = wx.Button(self, -1, 'Add Recipe')
+        hsizer.Add(self.b_add_recipe, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 2)
+        self.b_add_recipe.Bind(wx.EVT_BUTTON, self.OnAddRecipe)
+        
+        vsizer.Add(hsizer, 0, wx.EXPAND, 0)
+        self.SetSizerAndFit(vsizer)
+        self._protocol_rules._updated.connect(self.update)
+    
+    def OnClear(self, wx_event=None):
+        raise NotImplementedError
+
+    def OnAddRecipe(self, wx_event=None):
+        raise NotImplementedError
+
+    def OnProtocolChoice(self, wx_event=None):
+        raise NotImplementedError
+
+    def OnPostChoice(self, wx_event=None):
+        raise NotImplementedError
+        
+    def _set_text_styling(self):
+        from wx import stc
+        ed = self.tRecipeText
+        ed.SetLexer(wx.stc.STC_LEX_YAML)
+
+        # Enable folding
+        ed.SetProperty("fold", "1")
+
+        # Highlight tab/space mixing (shouldn't be any)
+        ed.SetProperty("tab.timmy.whinge.level", "1")
+
+        # Set left and right margins
+        ed.SetMargins(2, 2)
+
+        # Set up the numbers in the margin for margin #1
+        ed.SetMarginType(1, wx.stc.STC_MARGIN_NUMBER)
+        # Reasonable value for, say, 4-5 digits using a mono font (40 pix)
+        ed.SetMarginWidth(1, 40)
+
+        # Indentation and tab stuff
+        ed.SetIndent(4)               # Proscribed indent size for wx
+        ed.SetIndentationGuides(True) # Show indent guides
+        ed.SetBackSpaceUnIndents(True)# Backspace unindents rather than delete 1 space
+        ed.SetTabIndents(True)        # Tab key indents
+        ed.SetTabWidth(4)             # Proscribed tab size for wx
+        ed.SetUseTabs(False)          # Use spaces rather than tabs, or
+        # TabTimmy will complain!    
+        # White space
+        ed.SetViewWhiteSpace(False)   # Don't view white space
+
+        # EOL: Since we are loading/saving ourselves, and the
+        # strings will always have \n's in them, set the STC to
+        # edit them that way.            
+        ed.SetEOLMode(wx.stc.STC_EOL_LF)
+        ed.SetViewEOL(False)
+
+        # No right-edge mode indicator
+        ed.SetEdgeMode(stc.STC_EDGE_NONE)
+
+        # Setup a margin to hold fold markers
+        ed.SetMarginType(2, stc.STC_MARGIN_SYMBOL)
+        ed.SetMarginMask(2, stc.STC_MASK_FOLDERS)
+        ed.SetMarginSensitive(2, True)
+        ed.SetMarginWidth(2, 12)
+
+        # and now set up the fold markers
+        ed.MarkerDefine(stc.STC_MARKNUM_FOLDEREND, stc.STC_MARK_BOXPLUSCONNECTED, "white", "black")
+        ed.MarkerDefine(stc.STC_MARKNUM_FOLDEROPENMID, stc.STC_MARK_BOXMINUSCONNECTED, "white", "black")
+        ed.MarkerDefine(stc.STC_MARKNUM_FOLDERMIDTAIL, stc.STC_MARK_TCORNER, "white", "black")
+        ed.MarkerDefine(stc.STC_MARKNUM_FOLDERTAIL, stc.STC_MARK_LCORNER, "white", "black")
+        ed.MarkerDefine(stc.STC_MARKNUM_FOLDERSUB, stc.STC_MARK_VLINE, "white", "black")
+        ed.MarkerDefine(stc.STC_MARKNUM_FOLDER, stc.STC_MARK_BOXPLUS, "white", "black")
+        ed.MarkerDefine(stc.STC_MARKNUM_FOLDEROPEN, stc.STC_MARK_BOXMINUS, "white", "black")
+
+        # Global default style
+        if wx.Platform == '__WXMSW__':
+            ed.StyleSetSpec(stc.STC_STYLE_DEFAULT,
+                              'fore:#000000,back:#FFFFFF,face:Courier New')
+        elif wx.Platform == '__WXMAC__':
+            # TODO: if this looks fine on Linux too, remove the Mac-specific case 
+            # and use this whenever OS != MSW.
+            ed.StyleSetSpec(stc.STC_STYLE_DEFAULT,
+                              'fore:#000000,back:#FFFFFF,face:Monaco')
+        else:
+            defsize = wx.SystemSettings.GetFont(wx.SYS_ANSI_FIXED_FONT).GetPointSize()
+            ed.StyleSetSpec(stc.STC_STYLE_DEFAULT,
+                              'fore:#000000,back:#FFFFFF,face:Courier,size:%d' % defsize)
+
+        # Clear styles and revert to default.
+        ed.StyleClearAll()
+
+        # Following style specs only indicate differences from default.
+        # The rest remains unchanged.
+
+        # Line numbers in margin
+        ed.StyleSetSpec(wx.stc.STC_STYLE_LINENUMBER, 'fore:#000000,back:#99A9C2')
+        # Highlighted brace
+        ed.StyleSetSpec(wx.stc.STC_STYLE_BRACELIGHT, 'fore:#00009D,back:#FFFF00')
+        # Unmatched brace
+        ed.StyleSetSpec(wx.stc.STC_STYLE_BRACEBAD, 'fore:#00009D,back:#FF0000')
+        # Indentation guide
+        ed.StyleSetSpec(wx.stc.STC_STYLE_INDENTGUIDE, "fore:#CDCDCD")
+
+        # YAML styles
+        ed.StyleSetSpec(wx.stc.STC_YAML_DEFAULT, 'fore:#000000')
+        ed.StyleSetSpec(wx.stc.STC_YAML_COMMENT, 'fore:#008000,back:#F0FFF0')
+        ed.StyleSetSpec(wx.stc.STC_YAML_NUMBER, 'fore:#0080F0')
+        ed.StyleSetSpec(wx.stc.STC_YAML_IDENTIFIER, 'fore:#80000')
+        ed.StyleSetSpec(wx.stc.STC_YAML_DOCUMENT, 'fore:#E0E000') #what is this?
+        ed.StyleSetSpec(wx.stc.STC_YAML_KEYWORD, 'fore:#000080,bold')
+        ed.StyleSetSpec(wx.stc.STC_YAML_ERROR, 'fore:#FE2020')
+        ed.StyleSetSpec(wx.stc.STC_YAML_OPERATOR, 'fore:#0000A0')
+        ed.StyleSetSpec(wx.stc.STC_YAML_REFERENCE, 'fore:#E0E000')
+        ed.StyleSetSpec(wx.stc.STC_YAML_TEXT, 'fore:#E0E000')
+
+        # Caret color
+        ed.SetCaretForeground("BLUE")
+        # Selection background
+        ed.SetSelBackground(1, '#66CCFF')
+        
+    def update(self, *args, **kwargs):
+        self.rule_plot.draw()
+        
+    def OnPick(self, event):
+        # FIXME - open rule in respective editing tab
+        raise NotImplementedError
+        from PYME.IO import tabular
+        k = event.artist._data
+        if not (isinstance(k, six.string_types)):
+            self.configureModule(k)
+        else:
+            outp = self.recipes.activeRecipe.namespace[k]
+            if isinstance(outp, ImageStack):
+                if not 'dsviewer' in dir(self.recipes):
+                    dv = ViewIm3D(outp, mode='lite')
+                else:
+                    if self.recipes.dsviewer.mode == 'visGUI':
+                        mode = 'visGUI'
+                    else:
+                        mode = 'lite'
+                                   
+                    dv = ViewIm3D(outp, mode=mode, glCanvas=self.recipes.dsviewer.glCanvas)
+                    
+            elif isinstance(outp, tabular.TabularBase):
+                from PYME.ui import recArrayView
+                f = recArrayView.ArrayFrame(outp, parent=self, title='Data table - %s' % k)
+                f.Show()
     
     @property
     def rule_chain(self):
@@ -189,7 +438,8 @@ class ChainedAnalysisPage(wx.Panel):
     def add_tile(self, rule_tile):
         self.rule_chain.rule_factories.append(rule_tile)
         self._protocol_rules._updated.send(self)
-    
+
+
 class SMLMChainedAnalysisPage(ChainedAnalysisPage):
     def __init__(self, parent, protocol_rules, recipe_manager, 
                  localization_settings, spool_controller, 
@@ -214,8 +464,55 @@ class SMLMChainedAnalysisPage(ChainedAnalysisPage):
         self._protocol_rules = protocol_rules
         self._selected_protocol = list(protocol_rules.keys())[0]
         self._recipe_manager = recipe_manager
+        self._localization_settings = localization_settings
         self._spool_controller = spool_controller
+
+        hsizer = wx.BoxSizer(wx.HORIZONTAL)
+        vsizer = wx.BoxSizer(wx.VERTICAL)
+
+        # associated protocol choicebox (default / others on top, then rest)
+        hsizer.Add(wx.StaticText(self, -1, 'Associated Protocol:'), 0, 
+                   wx.LEFT|wx.RIGHT, 5)
+        self.c_protocol = wx.Choice(self, -1, choices=get_protocol_list())
+        self.c_protocol.SetSelection(0)
+        self.c_protocol.Bind(wx.EVT_CHOICE, self.OnProtocolChoice)
+        hsizer.Add(self.c_protocol, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 2)
+
+        # post on choicebox
+        hsizer.Add(wx.StaticText(self, -1, 'Post On:'), 0, 
+                   wx.LEFT|wx.RIGHT, 5)
+        self.c_post = wx.Choice(self, -1, choices=POST_CHOICES)
+        self.c_post.SetSelection(0)
+        self.c_post.Bind(wx.EVT_CHOICE, self.OnPostChoice)
+        hsizer.Add(self.c_post, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 2)
+
+        vsizer.Add(hsizer, 0)
+
+        # rule plot
+        self.rule_plot = RulePlotPanel(self, self._protocol_rules, 
+                                       size=(-1, 400))
+        vsizer.Add(self.rule_plot, 1, wx.ALL|wx.EXPAND, 5)
         
+        hsizer = wx.BoxSizer(wx.HORIZONTAL)
+        
+        self.b_clear = wx.Button(self, -1, 'Clear Chain')
+        hsizer.Add(self.b_clear, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 2)
+        self.b_clear.Bind(wx.EVT_BUTTON, self.OnClear)
+
+        self.b_add_recipe = wx.Button(self, -1, 'Add Recipe')
+        hsizer.Add(self.b_add_recipe, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 2)
+        self.b_add_recipe.Bind(wx.EVT_BUTTON, self.OnAddRecipe)
+        
+        self.b_add_localization = wx.Button(self, -1, 'Add Localization')
+        hsizer.Add(self.b_add_localization, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL, 2)
+        self.b_add_localization.Bind(wx.EVT_BUTTON, self.OnAddLocalization)
+        
+        vsizer.Add(hsizer, 0, wx.EXPAND, 0)
+        self.SetSizerAndFit(vsizer)
+        self._protocol_rules._updated.connect(self.update)
+    
+    def OnAddLocalization(self, wx_event=None):
+        pass
 
 class ChainedAnalysisPanel(wx.Panel):
     def __init__(self, parent, protocol_rules, spool_controller, 
