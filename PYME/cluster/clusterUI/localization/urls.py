a4bdc9fd45245a548361983cf31989cd27e69ade
David Baddeley <david.baddeley.nz@gmail.com>
2021-01-12 14:22:19 +1300

    update url specs for more recent django versions

--- PYME/cluster/clusterUI/localization/urls.py
+++ PYME/cluster/clusterUI/localization/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
 
+app_name='localization'
+
 urlpatterns = [
     url(r'^settings/$', views.settings, name='analysis_settings'),
     url(r'^settings/(?P<analysisModule>.+)/$', views.settings, name='analysis_settings'),
