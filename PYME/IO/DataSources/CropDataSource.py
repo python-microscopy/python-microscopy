c99f61934c534f968ba48c9a73d4720572c75e44
barentine <andrew.barentine@gmail.com>
2021-02-09 12:58:29 -0500

    similarly let the upper bound be the og stack shape

--- PYME/IO/DataSources/CropDataSource.py
+++ PYME/IO/DataSources/CropDataSource.py
@@ -39,7 +39,7 @@ class DataSource(BaseDataSource):
             x1 = xrange[1] if xrange[1] > -1 else self.dataSource.shape[0] + xrange[1]
             assert x1 - x0 > 0 and x1 - x0 <= self.dataSource.shape[0]
             assert x0 < self.dataSource.shape[0] and x0 >= 0
-            assert x1 < self.dataSource.shape[0] and x1 > 0
+            assert x1 <= self.dataSource.shape[0] and x1 > 0
             self.xrange = (x0, x1)
             
         if yrange is None or tuple(yrange) == (0, -1):
@@ -50,7 +50,7 @@ class DataSource(BaseDataSource):
             y1 = yrange[1] if yrange[1] > -1 else self.dataSource.shape[1] + yrange[1]
             assert y1 - y0 > 0 and y1 - y0 <= self.dataSource.shape[1]
             assert y0 < self.dataSource.shape[1] and y0 >= 0
-            assert y1 < self.dataSource.shape[1] and y1 > 0
+            assert y1 <= self.dataSource.shape[1] and y1 > 0
             self.yrange = (y0, y1)
             
         if trange is None or tuple(trange) == (0, -1):
@@ -62,7 +62,7 @@ class DataSource(BaseDataSource):
             t1 = trange[1] if trange[1] > -1 else n_slices + trange[1]
             assert t1 - t0 > 0 and t1 - t0 <= n_slices
             assert t0 < n_slices and t0 >= 0
-            assert t1 < n_slices and t1 > 0
+            assert t1 <= n_slices and t1 > 0
             self.trange = (t0, t1)
         
     
