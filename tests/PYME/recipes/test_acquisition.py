d738625c626c482b9037cee0cd731ed98ee33c20
David Baddeley <david.baddeley.nz@gmail.com>
2021-02-16 16:55:08 +1300

    fix action tests

--- tests/PYME/recipes/test_acquisition.py
+++ tests/PYME/recipes/test_acquisition.py
@@ -5,11 +5,19 @@ from PYME.Acquire.ActionManager import ActionManager, ActionManagerServer
 class fakescope(object):
     pass
 
+action_manager = None
+action_manager_server= None
+
 def setup():
+    global action_manager, action_manager_server
     scope = fakescope()
-    am = ActionManager(scope)
-    ams = ActionManagerServer(am, 9393, '127.0.0.1')
-    return am, ams
+    action_manager = ActionManager(scope)
+    action_manager_server = ActionManagerServer(action_manager, 9393, '127.0.0.1')
+    #return am, ams
+    
+def teardown():
+    action_manager_server.shutdown()
+    
 
 def test_queue_acquisitions():
     from PYME.IO.tabular import DictSource
@@ -17,17 +25,15 @@ def test_queue_acquisitions():
     import numpy as np
     import time
 
-    try:
-        action_manager, action_manager_server = setup()
-        action_manager.paused = True
+    action_manager.paused = True
 
-        d = DictSource({'x': np.arange(10), 'y': np.arange(10)})
-        rec = ModuleCollection()
-        rec.namespace['input'] = d
+    d = DictSource({'x': np.arange(10), 'y': np.arange(10)})
+    rec = ModuleCollection()
+    rec.namespace['input'] = d
 
-        rec.add_module(acquisition.QueueAcquisitions(rec))
-        rec.save()
-        time.sleep(1)
-        task = action_manager.actionQueue.get_nowait()
-    finally:
-        action_manager_server.shutdown()
+    rec.add_module(acquisition.QueueAcquisitions(rec))
+    rec.save()
+    time.sleep(1)
+    task = action_manager.actionQueue.get_nowait()
+    
+    
