de0b5e693ff0baa397abe761816ed13dbb2802cc
zacsimile <zachcm@gmail.com>
2021-05-05 19:03:22 -0400

    Improve speed and accuracy of distance_to_mesh

--- PYME/experimental/isosurface.py
+++ PYME/experimental/isosurface.py
@@ -29,26 +29,22 @@ def isosurface(data, isolevel, voxel_size=None, origin=None, remesh=False):
     return T
 
 def triangle_sdf(p, pv):
-    p1p0 = pv[:,1] - pv[:,0]
-    p2p1 = pv[:,2] - pv[:,1]
-    p0p2 = pv[:,0] - pv[:,2]
-    pp0 = p - pv[:,0]
-    pp1 = p - pv[:,1]
-    pp2 = p - pv[:,2]
-    n = np.cross(p1p0, p0p2, axis=1)
+    """point plane distance
 
-    s1 = np.sign((np.cross(p1p0,n,axis=1)*pp0).sum(1))
-    s2 = np.sign((np.cross(p2p1,n,axis=1)*pp1).sum(1))
-    s3 = np.sign((np.cross(p0p2,n,axis=1)*pp2).sum(1))
-
-    f = (n*pp0).sum(1)*(n*pp0).sum(1)/(n*n).sum(1)
-    sign_mask = (s1+s2+s3) < 2
-    f[sign_mask] = np.minimum(np.minimum(
-                    ((p1p0*np.clip((p2p1*pp0).sum(1)/(p1p0*p1p0).sum(1),0.0,1.0)[:,None]-pp0)**2).sum(1),
-                    ((p2p1*np.clip((p2p1*pp1).sum(1)/(p2p1*p2p1).sum(1),0.0,1.0)[:,None]-pp1)**2).sum(1)),
-                    ((p0p2*np.clip((p0p2*pp2).sum(1)/(p0p2*p0p2).sum(1),0.0,1.0)[:,None]-pp2)**2).sum(1))[sign_mask]
-
-    return np.sign((pp0*n).sum(1))*np.sqrt(f)
+    Parameters
+    ----------
+    p : np.array
+        Nx3 points
+    pv : np.array
+        3x3 triangle vertex x coordinates
+    """
+    p1p0 = pv[:,:,1] - pv[:,:,0]
+    p2p0 = pv[:,:,2] - pv[:,:,0]
+    n = np.cross(p1p0,p2p0,axis=2)
+    nn = np.linalg.norm(n,axis=2)
+    nh = n/nn[...,None]
+    nh[nn==0] = 0
+    return (-nh*(p[:,None,:]-pv[:,:,0])).sum(1)
 
 def distance_to_mesh(points, surf):
     """
@@ -78,13 +74,5 @@ def distance_to_mesh(points, surf):
     _v = surf.faces[_faces]
     v = surf._vertices['position'][_v]  # (points, N, (v0,v1,v2), (x,y,z))
 
-    # Vector of distances
-    d = np.zeros(v.shape[0])
-
-    # Calculate planar distances from each point to its candidate triangles
-    # and choose the minimum absolute value
-    for _i in range(v.shape[0]):
-        d_tmp = triangle_sdf(points[_i], v[_i,...])
-        d[_i] = d_tmp[np.argmin(np.abs(d_tmp))]
-
-    return d
+    # Intersect (np.max) SDFs of nearest triangles
+    return np.max(triangle_sdf(points, v),axis=1)
