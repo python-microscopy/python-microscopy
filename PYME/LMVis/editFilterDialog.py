d98f8b8350b605ece619228784aa130c11c953a7
zacsimile <zach.marin@yale.edu>
2021-06-04 16:26:48 -0400

    More bad alignments

--- PYME/LMVis/editFilterDialog.py
+++ PYME/LMVis/editFilterDialog.py
@@ -53,7 +53,7 @@ class FilterEditDialog(wx.Dialog):
 
         sizer2.Add(self.tMax, 0, wx.ALIGN_CENTER_VERTICAL | wx.ALL, 5)
 
-        sizer1.Add(sizer2, 0, wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5)
+        sizer1.Add(sizer2, 0, wx.ALL, 5)
 
         
         btSizer = wx.StdDialogButtonSizer()
@@ -69,7 +69,7 @@ class FilterEditDialog(wx.Dialog):
 
         btSizer.Realize()
 
-        sizer1.Add(btSizer, 0, wx.ALIGN_RIGHT|wx.ALIGN_CENTER_VERTICAL|wx.ALL, 5)
+        sizer1.Add(btSizer, 0, wx.ALIGN_RIGHT|wx.ALL, 5)
 
         self.SetSizer(sizer1)
         sizer1.Fit(self)
