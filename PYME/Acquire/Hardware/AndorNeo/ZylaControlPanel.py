fd3df609d8bbe55c3393355f92443b426b726232
David Baddeley <david.baddeley.nz@gmail.com>
2021-09-06 09:56:52 +1200

    naming tweaks

--- PYME/Acquire/Hardware/AndorNeo/ZylaControlPanel.py
+++ PYME/Acquire/Hardware/AndorNeo/ZylaControlPanel.py
@@ -8,24 +8,26 @@ Created on Wed Apr 01 16:29:38 2015
 import wx
 
 class EnumControl(wx.Panel):
-    def __init__(self, parent, target, targetPropertyName=None):
+    def __init__(self, parent, target, display_name=None):
         wx.Panel.__init__(self, parent)
         self.scope = parent.scope
         self.parent = parent
         self.target = target
+        
         # this keyword allows us to override unwieldy property Names
-        if targetPropertyName is None:
-            self.targetPropertyName = target.propertyName
+        if display_name is None:
+            self.display_name = target.propertyName
         else:
-            self.targetPropertyName = targetPropertyName
+            self.display_name = display_name
             
+        # TODO - do we really need to support read only enums????
         try:
-            self.targetReadOnly = target.isReadOnly()
+            self._read_only = target.isReadOnly()
         except AttributeError:
-            self.targetReadOnly = False
+            self._read_only = False
         
         hsizer = wx.BoxSizer(wx.HORIZONTAL)
-        hsizer.Add(wx.StaticText(self, -1, self.targetPropertyName), 1, wx.ALIGN_CENTER_VERTICAL|wx.ALL, 2)
+        hsizer.Add(wx.StaticText(self, -1, self.display_name), 1, wx.ALIGN_CENTER_VERTICAL|wx.ALL, 2)
         
         self.cChoice = wx.Choice(self, -1, size = [100,-1])
         self.cChoice.Bind(wx.EVT_CHOICE, self.onChange)
@@ -36,7 +38,7 @@ class EnumControl(wx.Panel):
         self.SetSizerAndFit(hsizer)
         
     def onChange(self, event=None):
-        if not self.targetReadOnly:
+        if not self._read_only:
             self.scope.frameWrangler.stop()
             self.target.setString(self.cChoice.GetStringSelection())
             self.scope.frameWrangler.start()
