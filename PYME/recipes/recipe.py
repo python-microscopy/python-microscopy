58daa02bbc23b4a989f971e9c6b764edd6fe074c
David Baddeley <david.baddeley.nz@gmail.com>
2022-08-26 11:23:23 +1200

    more informative error for missing modules

--- PYME/recipes/recipe.py
+++ PYME/recipes/recipe.py
@@ -7,6 +7,10 @@ from PYME.IO.image import ImageStack
 import logging
 logger = logging.getLogger(__name__)
 
+#custom error for typos in recipe module names
+class RecipeModuleNotFound(Exception):
+    pass
+
 class Recipe(HasTraits):
     modules = List()
     execute_on_invalidation = Bool(False)
@@ -359,7 +363,7 @@ class Recipe(HasTraits):
                 try:
                     mod = base._legacy_modules[mn.split('.')[-1]](self, **md)
                 except KeyError:
-                    raise KeyError('No recipe module found with name "%s"' % mn)
+                    raise RecipeModuleNotFound('No recipe module found with name "%s"\nThis could either be caused by a typo in the module name or a missing 3rd party plugin.' % mn) from None # Full traceback is probably unhelpful for people using modules 
             
             mc.append(mod)
         
