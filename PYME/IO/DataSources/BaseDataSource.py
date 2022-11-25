c4480d78a73a412e8060d4fc11deae8a179bfd71
David Baddeley <david.baddeley.nz@gmail.com>
2022-11-25 14:28:21 +1300

    fix for error mentioned in #1262

--- PYME/IO/DataSources/BaseDataSource.py
+++ PYME/IO/DataSources/BaseDataSource.py
@@ -337,7 +337,7 @@ class XYZTCDataSource(BaseDataSource):
         #print(indices)
         #allocate output array
         out = np.zeros([_slice_len(slice(*indices[k])) for k in 'XYZTC'], dtype=self.dtype)
-        print(out.shape)
+        #print(indices, out.shape)
         
         for ci, c in enumerate(range(*indices['C'])):
             for ti, t in enumerate(range(*indices['T'])):
