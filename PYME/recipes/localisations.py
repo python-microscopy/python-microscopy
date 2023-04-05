9fd3e9afde775aa2c839d9257026126158a5d42e
Christian Soeller <c.soeller@gmail.com>
2023-04-05 19:57:14 +0200

    add discard trivial switch to MergeClumps module

--- PYME/recipes/localisations.py
+++ PYME/recipes/localisations.py
@@ -352,13 +352,14 @@ class MergeClumps(ModuleBase):
     inputName = Input('clumped')
     outputName = Output('merged')
     labelKey = CStr('clumpIndex')
+    discardTrivial = Bool(False)
 
     def execute(self, namespace):
         from PYME.Analysis.points.DeClump import pyDeClump
 
         inp = namespace[self.inputName]
 
-        grouped = pyDeClump.mergeClumps(inp, labelKey=self.labelKey)
+        grouped = pyDeClump.mergeClumps(inp, labelKey=self.labelKey, discard_trivial=self.discardTrivial)
         try:
             grouped.mdh = inp.mdh
         except AttributeError:
