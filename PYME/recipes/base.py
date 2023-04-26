72a04d4ddd3541724198dc6365512dd657f766c9
David Baddeley <david.baddeley.nz@gmail.com>
2023-04-27 10:49:37 +1200

    recipe metadata bugfix

--- PYME/recipes/base.py
+++ PYME/recipes/base.py
@@ -277,7 +277,7 @@ class ModuleBase(HasStrictTraits):
         self._params_to_metadata(mdh)
 
         for v in out.values():
-            if not hasattr(v, 'mdh'):
+            if getattr(v, 'mdh', None) is None:
                 v.mdh = MetaDataHandler.DictMDHandler()
 
             v.mdh.mergeEntriesFrom(mdhin) #merge, to allow e.g. voxel size overrides due to downsampling
