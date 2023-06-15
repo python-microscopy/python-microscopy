4a98dc45ddd0dd280e0d1ec593483e80bda184a3
David Baddeley <david.baddeley.nz@gmail.com>
2023-06-15 21:15:31 +1200

    fix for django>2.1 (tested up to 4.1)

--- PYME/cluster/clusterUI/clusterstatus/urls.py
+++ PYME/cluster/clusterUI/clusterstatus/urls.py
@@ -1,8 +1,9 @@
-from django.conf.urls import url
+#from django.conf.urls import url
+from django.urls import re_path
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.status, name='status'),
-    url(r'^queues/$', views.queues, name='queues'),
-    url(r'^load/$', views.load, name='load'),
+    re_path(r'^$', views.status, name='status'),
+    re_path(r'^queues/$', views.queues, name='queues'),
+    re_path(r'^load/$', views.load, name='load'),
 ]
--- PYME/cluster/clusterUI/recipes/urls.py
+++ PYME/cluster/clusterUI/recipes/urls.py
@@ -1,17 +1,17 @@
-from django.conf.urls import url
+from django.urls import re_path
 from . import views
 
 app_name='recipes'
 
 urlpatterns = [
-    url(r'^simple_form/$', views.recipe_form, name='recipe'),
-    url(r'^run/$', views.run, name='runrecipe'),
-    url(r'^run_template/$', views.run_template, name='runtemplate'),
-    url(r'^svg/$', views.view_svg, name='svg'),
-    url(r'^standalone/$', views.recipe_standalone, name='bakeshop'),
-    url(r'^editor/$', views.recipe_editor, name='editor'),
-    url(r'^editrun/$', views.recipe_editrun, name='editrun'),
-    url(r'^template/$', views.recipe_template, name='recipetemplate'),
-    url(r'^find_inputs$', views.get_input_glob, name='find_inputs'),
-    url(r'^extra_inputs/$', views.extra_inputs, name='extra_inputs'),
+    re_path(r'^simple_form/$', views.recipe_form, name='recipe'),
+    re_path(r'^run/$', views.run, name='runrecipe'),
+    re_path(r'^run_template/$', views.run_template, name='runtemplate'),
+    re_path(r'^svg/$', views.view_svg, name='svg'),
+    re_path(r'^standalone/$', views.recipe_standalone, name='bakeshop'),
+    re_path(r'^editor/$', views.recipe_editor, name='editor'),
+    re_path(r'^editrun/$', views.recipe_editrun, name='editrun'),
+    re_path(r'^template/$', views.recipe_template, name='recipetemplate'),
+    re_path(r'^find_inputs$', views.get_input_glob, name='find_inputs'),
+    re_path(r'^extra_inputs/$', views.extra_inputs, name='extra_inputs'),
 ]
