895c904cd15fc883db5f3b5ec6abaabab4c2b9a0
csoeller <c.soeller@gmail.com>
2023-10-12 13:06:00 +0200

    Update PYME/LMVis/visCore.py
    Co-authored-by: David Baddeley <19475296+David-Baddeley@users.noreply.github.com>

--- PYME/LMVis/visCore.py
+++ PYME/LMVis/visCore.py
@@ -127,7 +127,12 @@ class VisGUICore(object):
         
     
     def OnIdle(self, event=None):
-        logger.debug('Ev Idle')
+       """ Refresh the glDisplay *after* all windows have been created and data loaded.
+       
+       TODO - rename, as the OnIdle name is a historical artifact (was originally called using an wx.EVT_IDLE binding, 
+       now uses wx.CallLater with a delay.
+       """
+        #logger.debug('Ev Idle')
         if self.glCanvas._is_initialized and not self.refv:
             self.refv = True
             logger.debug((self.viewMode, self.pointDisplaySettings.colourDataKey))
