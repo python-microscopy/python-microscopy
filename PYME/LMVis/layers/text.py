4b5ca38b14ce01d71878baddebfeac3ef80746ff
David Baddeley <david.baddeley.nz@gmail.com>
2024-07-23 19:40:01 +1200

    fix text overlays for gl core profile

--- PYME/LMVis/layers/text.py
+++ PYME/LMVis/layers/text.py
@@ -99,13 +99,13 @@ class Text(BaseEngine):
             glActiveTexture(GL_TEXTURE0)
             glBindTexture(GL_TEXTURE_2D, self._texture_id)
             glPixelStorei(GL_UNPACK_ALIGNMENT, 1)
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP)
-            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP)
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE)
+            glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE)
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST)
             #glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR)
             glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST)
             #glTexParameteri (GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR)
-            glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE)
+            #glTexEnvf(GL_TEXTURE_ENV, GL_TEXTURE_ENV_MODE, GL_REPLACE)
             glTexImage2D(GL_TEXTURE_2D, 0, GL_R32F, image.shape[1], image.shape[0], 0, GL_RED, GL_FLOAT,
                          image.astype('f4'))
     
@@ -113,67 +113,136 @@ class Text(BaseEngine):
         im, h, w = self.get_img_array(gl_canvas)
         with self.get_shader_program(gl_canvas) as sp:
             self.set_texture(im)
+            
+            if gl_canvas.core_profile:
+                import glm
+                # find the on-screen position of our text
+                vp = glGetIntegerv(GL_VIEWPORT)
+                #print('vp:', vp)
 
-            mv = glGetDoublev(GL_MODELVIEW_MATRIX)
-            p = glGetDoublev(GL_PROJECTION_MATRIX)
-            vp = glGetIntegerv(GL_VIEWPORT)
+                pos = np.zeros(3)
+                pos[:len(self.pos)] = self.pos
+                x0, y0, _, _ = gl_canvas.mvp*glm.vec4(*pos, 1)
 
-            #print(mv, p, vp)
+                #print('textPos:', x0, y0)
+                # convert to screen pixel coordinates
 
-            # calculate on-screen pixel coordinates for our text (perform model-view transformation)
-            pos = np.zeros(3)
-            pos[:len(self.pos)] = self.pos
-            x0, y0, _ = GLU.gluProject(pos[0], pos[1], pos[2], mv, p, vp)
+                x0  = (x0 + 1.) * vp[2] / 2.
+                y0 = (y0 + 1.) * vp[3] / 2.
 
-            #print('textPos:', x0, y0)
+                #print('textPos (px):', x0, y0)
 
-            try:
-                # set model-view so that we are drawing in screen pixel coordinates
-                glMatrixMode(GL_PROJECTION)
-                glPushMatrix()
-                glLoadIdentity()
-                glOrtho(0, vp[2], 0, vp[3], -1000, 1000)
+                verts = self._gen_rect_triangles(x0, y0, w, -h)
+                tex_coords = self._gen_rect_texture_coords()
 
-                glMatrixMode(GL_MODELVIEW)
-                glPushMatrix()
-                glLoadIdentity()
+                if self._color is None:
+                    cols = np.ones([6, 4], dtype='f4')
+                else:
+                    cols = np.repeat(self._color, 6, axis=0)
 
-        
-                glEnable(GL_TEXTURE_2D) # enable texture mapping */
+                proj = glm.ortho(0, vp[2], 0, vp[3], -1000, 1000)
+                #print('proj:', proj)
+                sp.set_modelviewprojectionmatrix(np.array(proj))
+
                 glActiveTexture(GL_TEXTURE0)
                 glBindTexture(GL_TEXTURE_2D, self._texture_id) # bind to our texture, has id of 1 */
                 glUniform1i(sp.get_uniform_location("im_sampler"), 0)
-            
-                # FIXME - choose appropriately for current viewport - want to make it so that the text renders real size
-                # (i.e. unwind any model-view / and projection stuff).
-                #x0, y0 = self.pos
-                x1 = x0 + w#*gl_canvas.content_scale_factor
-                y1 = y0 - h#*gl_canvas.content_scale_factor
-            
-                glDisable(GL_TEXTURE_GEN_S)
-                glDisable(GL_TEXTURE_GEN_T)
-                glDisable(GL_TEXTURE_GEN_R)
-            
-                #glColor3f(1.,0.,0.)
-                if self._color is not None:
-                    glColor4f(*self._color)
-                else:
-                    glColor4f(1., 1., 1., 1.)
+
+                vao = glGenVertexArrays(1)
+                vbo = glGenBuffers(3)
+                glBindVertexArray(vao)
+                glBindBuffer(GL_ARRAY_BUFFER, vbo[0])
+                glBufferData(GL_ARRAY_BUFFER, verts, GL_STATIC_DRAW)
+                glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, None)
+                glEnableVertexAttribArray(0)
+                glBindBuffer(GL_ARRAY_BUFFER, vbo[1])
+                glBufferData(GL_ARRAY_BUFFER, tex_coords, GL_STATIC_DRAW)
+                glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, None)
+                glEnableVertexAttribArray(1)
+                glBindBuffer(GL_ARRAY_BUFFER, vbo[2])
+                glBufferData(GL_ARRAY_BUFFER, cols, GL_STATIC_DRAW)
+                glVertexAttribPointer(2, 4, GL_FLOAT, GL_FALSE, 0, None)
+                glEnableVertexAttribArray(2)
+
+                glPolygonMode(GL_FRONT_AND_BACK, GL_FILL)
+                glDrawArrays(GL_TRIANGLES, 0, 6)
+
+                glBindVertexArray(0)
+                glDeleteVertexArrays(1, vao)
+                glDeleteBuffers(3, vbo)
+
+            else:
+                #legacy mode
+                mv = glGetDoublev(GL_MODELVIEW_MATRIX)
+                p = glGetDoublev(GL_PROJECTION_MATRIX)
+                
+
+                #print(mv, p, vp)
+
+                # calculate on-screen pixel coordinates for our text (perform model-view transformation)
+                pos = np.zeros(3)
+                pos[:len(self.pos)] = self.pos
+                x0, y0, _ = GLU.gluProject(pos[0], pos[1], pos[2], mv, p, vp)
+
+                #print('textPos:', x0, y0)
+
+                try:
+                    # set model-view so that we are drawing in screen pixel coordinates
                     
-                glBegin(GL_QUADS)
-                glTexCoord2f(0., 0.) # lower left corner of image */
-                glVertex3f(x0, y0, 0.0)
-                glTexCoord2f(1., 0.) # lower right corner of image */
-                glVertex3f(x1, y0, 0.0)
-                glTexCoord2f(1.0, 1.0) # upper right corner of image */
-                glVertex3f(x1, y1, 0.0)
-                glTexCoord2f(0.0, 1.0) # upper left corner of image */
-                glVertex3f(x0, y1, 0.0)
-                glEnd()
+                    glMatrixMode(GL_PROJECTION)
+                    glPushMatrix()
+                    glLoadIdentity()
+                    glOrtho(0, vp[2], 0, vp[3], -1000, 1000)
+
+                    glMatrixMode(GL_MODELVIEW)
+                    glPushMatrix()
+                    glLoadIdentity()
+
             
-                glDisable(GL_TEXTURE_2D)
-            finally:
-                glMatrixMode(GL_PROJECTION)
-                glPopMatrix()
-                glMatrixMode(GL_MODELVIEW)
-                glPopMatrix()
+                    glEnable(GL_TEXTURE_2D) # enable texture mapping */
+                    glActiveTexture(GL_TEXTURE0)
+                    glBindTexture(GL_TEXTURE_2D, self._texture_id) # bind to our texture, has id of 1 */
+                    glUniform1i(sp.get_uniform_location("im_sampler"), 0)
+                
+                    # FIXME - choose appropriately for current viewport - want to make it so that the text renders real size
+                    # (i.e. unwind any model-view / and projection stuff).
+                    #x0, y0 = self.pos
+                    x1 = x0 + w#*gl_canvas.content_scale_factor
+                    y1 = y0 - h#*gl_canvas.content_scale_factor
+                
+                    glDisable(GL_TEXTURE_GEN_S)
+                    glDisable(GL_TEXTURE_GEN_T)
+                    glDisable(GL_TEXTURE_GEN_R)
+                
+                    #glColor3f(1.,0.,0.)
+                    if self._color is not None:
+                        glColor4f(*self._color)
+                    else:
+                        glColor4f(1., 1., 1., 1.)
+
+                    #vertex_data = np.array([[x0, y0, 0.0], 
+                    #                        [x1, y0, 0.0], 
+                    #                        [x1, y1, 0.0], 
+                    #                        [x0, y1, 0.0]], dtype='f4') 
+
+                    #_vbo = glGenBuffers(1)
+                    #glBindBuffer(GL_ARRAY_BUFFER, _vbo)
+                    #glBufferData(GL_ARRAY_BUFFER, vertex_data, GL_STATIC_DRAW)   
+
+                    glBegin(GL_QUADS)
+                    glTexCoord2f(0., 0.) # lower left corner of image */
+                    glVertex3f(x0, y0, 0.0)
+                    glTexCoord2f(1., 0.) # lower right corner of image */
+                    glVertex3f(x1, y0, 0.0)
+                    glTexCoord2f(1.0, 1.0) # upper right corner of image */
+                    glVertex3f(x1, y1, 0.0)
+                    glTexCoord2f(0.0, 1.0) # upper left corner of image */
+                    glVertex3f(x0, y1, 0.0)
+                    glEnd()
+                
+                    glDisable(GL_TEXTURE_2D)
+                finally:
+                    glMatrixMode(GL_PROJECTION)
+                    glPopMatrix()
+                    glMatrixMode(GL_MODELVIEW)
+                    glPopMatrix()
--- PYME/LMVis/shader_programs/shaders/text_fs.glsl
+++ PYME/LMVis/shader_programs/shaders/text_fs.glsl
@@ -17,3 +17,26 @@ void main() {
 
     gl_FragColor = vec4(gl_Color.xyz, I);
 }
+
+#version 330
+//This is a very simple shader. It simply forwards the given color to the fragment.
+//There's no shading involved.
+in float vis;
+in vec2 tex_coords;
+in vec4 frontColor;
+
+uniform sampler2D im_sampler;
+
+out vec4 fragColor;
+
+void main() {
+    //gl_FragColor = vec4(1.0, 1.0, 0.0, 1.0);
+    //if (vis < .5) discard;
+
+    float I;
+
+    I = texture(im_sampler, tex_coords).r;
+    I = clamp(I, 0.0, 1.0);
+
+    fragColor = vec4(frontColor.xyz, I);
+}
