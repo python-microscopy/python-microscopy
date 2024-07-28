63d2dedbf3385a25b1ab2188933b30727ca1ecf9
David Baddeley <david.baddeley.nz@gmail.com>
2024-07-29 10:22:18 +1200

    add geometry shader for wide lines

---
+++ PYME/LMVis/shader_programs/shaders/widelines_gs.glsl
@@ -0,0 +1,50 @@
+#version 330 core
+// convert points to quads by using a geometry shader
+
+uniform float line_width_px;
+uniform vec2 viewport_size;
+
+layout (lines) in;
+layout (triangle_strip, max_vertices = 4) out;
+
+in vec4 FrontColor[];
+in vec2 normal[]; // this is not atually the normal, but a vector which is the average of the line perpendiculars for the current and previous/next line segments.
+in float vis[];
+
+out vec4 frontColor;
+out vec2 texCoord;
+out float visibility;
+
+void main(){
+    float half_size =  line_width_px/ (2.0*viewport_size.x);
+    vec4 pos0 = gl_in[0].gl_Position;
+    vec4 pos1 = gl_in[1].gl_Position;
+
+    float aspect = viewport_size.x / viewport_size.y;
+
+    vec2 dir = normalize(pos1.xy - pos0.xy);
+
+    vec2 ortho = vec2(-dir.y, dir.x*aspect);
+
+    vec2 offset0 = normal[0] * half_size / dot(normal[0], ortho);
+    vec2 offset1 = normal[1] * half_size / dot(normal[1], ortho);
+
+    // pass through colour and visibility
+    frontColor = FrontColor[0];
+    visibility = vis[0];
+
+    gl_Position = pos0 - vec4(offset0, 0, 0);    // 1:bottom-left
+    texCoord = vec2(0.0, 0.0);
+    EmitVertex();   
+    gl_Position = pos1 - vec4(offset1, 0.0, 0.0);    // 2:bottom-right
+    texCoord = vec2(1.0, 0.0);
+    EmitVertex();
+    gl_Position = pos0 + vec4(offset0, 0.0, 0.0);    // 3:top-left
+    texCoord = vec2(0.0, 1.0);
+    EmitVertex();
+    gl_Position = pos1 + vec4(offset1, 0.0, 0.0);    // 4:top-right
+    texCoord = vec2(1.0, 1.0);
+    EmitVertex();
+    EndPrimitive();
+
+}
