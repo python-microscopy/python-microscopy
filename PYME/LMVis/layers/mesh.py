fac2a8521a6dc858767300982280a5aeb8fc894f
Christian Soeller <c.soeller@gmail.com>
2025-02-21 12:00:44 +0100

    more traits 7.x fixes

--- PYME/LMVis/layers/mesh.py
+++ PYME/LMVis/layers/mesh.py
@@ -183,7 +183,10 @@ class TriangleRenderLayer(EngineLayer):
         self.on_trait_change(self._set_method, 'method')
 
         # update any of our traits which were passed as command line arguments
-        self.set(**kwargs)
+        try:
+            self.set(**kwargs)
+        except AttributeError:
+            self.trait_set(**kwargs)
 
         # update datasource and method
         self.dsname = dsname
--- PYME/LMVis/layers/quiver.py
+++ PYME/LMVis/layers/quiver.py
@@ -109,7 +109,10 @@ class QuiverRenderLayer(EngineLayer):
         self.on_trait_change(self._set_method, 'method')
 
         # update any of our traits which were passed as command line arguments
-        self.set(**kwargs)
+        try:
+            self.set(**kwargs)
+        except AttributeError:
+            self.trait_set(**kwargs)
 
         # update datasource and method
         self.dsname = dsname
