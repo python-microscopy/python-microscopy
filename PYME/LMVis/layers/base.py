e1203dfae6dae79e623770beb5f4a5826ed8c81b
Christian Soeller <c.soeller@gmail.com>
2025-02-22 15:44:22 +0100

    a few more self.get/set fixes for traits 7.x

--- PYME/LMVis/layers/base.py
+++ PYME/LMVis/layers/base.py
@@ -223,7 +223,11 @@ class BaseLayer(HasTraits):
         pass
 
     def settings_dict(self):
-        d =  self.get([n for n in self.class_trait_names() if not n.startswith('_')])
+        params = [n for n in self.class_trait_names() if not n.startswith('_')]
+        try:
+            d =  self.get(params)
+        except AttributeError:
+            d = self.trait_get(params)
         for k, v in d.items():
             if isinstance(v, dict) and not type(v) == dict:
                 v = dict(v)
