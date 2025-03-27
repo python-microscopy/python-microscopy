191f71871953ce2b8e1082ad7e4fd21f97108d19
David Baddeley <19475296+David-Baddeley@users.noreply.github.com>
2025-03-28 11:18:08 +1300

    add fixmes for DictStr etc

--- PYME/recipes/traits.py
+++ PYME/recipes/traits.py
@@ -12,6 +12,7 @@ except ImportError:
 
 # definitions below have been adapted based on https://docs.enthought.com/traits-futures/0.2/_modules/traits/trait_types.html
 # these Dict have been deprecated for a while and need to be defined here for newer traits.api modules
+# FIXME - change usage within code and remove aliases
 DictStrAny = Dict(str, Any)
 DictStrStr = Dict(str, str)
 DictStrList = Dict(str, list)
@@ -19,6 +20,7 @@ DictStrFloat = Dict(str, float)
 DictStrBool = Dict(str, bool)
 
 # -- List Traits ------- same as dict traits; we define only a subset of all possible List traits here
+# FIXME - change usage within code and remove aliases
 ListInt = List(int)
 ListFloat = List(float)
 ListStr = List(str)
