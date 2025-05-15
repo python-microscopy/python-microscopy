f5fd775f51877bf18b8b191c15001b03d5d08b02
David Baddeley <19475296+David-Baddeley@users.noreply.github.com>
2025-05-16 11:29:08 +1200

    Update test_recipes.py

--- tests/PYME/recipes/test_recipes.py
+++ tests/PYME/recipes/test_recipes.py
@@ -69,7 +69,17 @@ def test_recipe_1():
     rec.execute(input=im)
     assert(np.allclose(rec.namespace['zoomed'].data_xyztc.shape, (88, 80, 241, 1, 2)))
 
-def test_recipe_2():
+def test_recipe_dict_trait_serialisation():
+    """ Extremely minimal test to ensure we can save a recipe to YAML
+    
+    - protects against massive breakages in saving (ie we have something which is not yaml serialisable)
+    - does NOT check that round-trip is accurate
+    - chosen recipe (recipe2) should specifically test the regression in #1592 (ie modified Dict traits)
+    - has really bad coverage on other recipe modules and trait types
+
+    TODO - should we find some way of validating round-trip success (can't directly compare strings as ordering, string escaping, and indentation not garuanteed to be preserved)
+    """
+    
     rec = Recipe.fromYAML(recipe_2)
     yaml_str = rec.toYAML()
     assert(yaml_str != '')
